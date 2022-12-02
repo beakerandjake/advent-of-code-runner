e46df7a434fd22376100fcaa8f1449cf09893e8d
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-02 14:25:59 -0700

    human readable stack trace

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,28 @@
 import { createLogger, format, transports } from 'winston';
 
+const printStackTrace = process.env.NODE_END !== 'production';
+
 export const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: format.splat(),
+  format: format.combine(
+    format.errors({ stack: printStackTrace }),
+    format.splat(),
+    format.json()
+  ),
   transports: [
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
+      format: format.combine(
+        format.colorize(),
+        // format.simple would be nice to use, but doesn't print a human readable stack trace.
+        // write a custom printf that mirrors format.simple, but prints a better stack trace.
+        format.printf(({ level, message, stack }) => {
+          if (printStackTrace) {
+            return `${level}: ${message} - ${stack}`;
+          }
+          return `${level}: ${message}`;
+        })
+      ),
+      handleExceptions: true,
     }),
   ],
 });
