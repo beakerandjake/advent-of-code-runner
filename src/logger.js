21e46162c9f79eabfa74384c2be99ca182c10225
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-02 18:27:33 -0700

    handle config failure in logging startup

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,26 @@
 import { createLogger, format, transports } from 'winston';
+import { getConfigValue } from './config.js';
 
-const printStackTrace = process.env.NODE_ENV !== 'production';
+const loggingConfig = {
+  level: 'info',
+  printStackTrace: false
+};
+
+// If for some reason configuration fails to load while setting up logger
+// ensure that this failure at least gets logged to the console.  
+
+try {
+  loggingConfig.level = getConfigValue('logging.level');
+  loggingConfig.printStackTrace = getConfigValue('logging.includeStackTrace');
+} catch (error) {
+  console.error('Failed to load logging config!');
+  throw error;
+}
 
 export const logger = createLogger({
-  level: process.env.LOG_LEVEL || 'info',
+  level: loggingConfig.level,
   format: format.combine(
-    format.errors({ stack: printStackTrace }),
+    format.errors({ stack: loggingConfig.printStackTrace }),
     format.splat(),
     format.json()
   ),
@@ -16,7 +31,7 @@ export const logger = createLogger({
         // format.simple would be nice to use, but doesn't print a human readable stack trace.
         // write a custom printf that mirrors format.simple, but prints a better stack trace.
         format.printf(({ level, message, stack }) => {
-          if (printStackTrace && stack) {
+          if (loggingConfig.printStackTrace && stack) {
             return `${level}: ${message} - ${stack}`;
           }
           return `${level}: ${message}`;
