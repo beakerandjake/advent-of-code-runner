3bd99f267e5e66abebdf14d1b69e69c6056096f8
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-02 17:12:28 -0700

    create directories

--- src/io.js
+++ src/io.js
@@ -1,7 +1,7 @@
-import { writeFile } from 'fs/promises';
+import { writeFile, mkdir } from 'fs/promises';
 import { logger } from './logger.js';
 import { cwd } from 'process';
-import { join } from 'path';
+import { join, dirname } from 'path';
 
 const INPUTS_FOLDER = join(cwd(), 'inputs');
 
@@ -15,6 +15,18 @@ const getFileName = (year, day) =>
   join(INPUTS_FOLDER, `${year}`, `day_${day}.txt`);
 
 /**
+ * Recursively creates all directories which do not exist. Existing directories will be skipped.
+ * @param {String} fileName 
+ */
+const ensureDirectoriesExist = async (fileName) => {
+  const directory = dirname(fileName);
+
+  logger.debug('ensuring directories exists: %s', directory);
+
+  await mkdir(directory, { recursive: true });
+};
+
+/**
  * Creates a file in the cwd with pattern "/{year}/day/day_{day}.txt"
  * If the file already exists it will be overwritten.
  * @param {Number} year
@@ -25,8 +37,10 @@ export const saveInputToFile = async (year, day, input) => {
   const fileName = getFileName(year, day);
 
   logger.info('saving input for year: %s, day: %s at: %s', year, day, fileName);
+
+  await ensureDirectoriesExist(fileName);
   
-  
+  logger.info('dirname %s', dirname(fileName));
 
   await writeFile(fileName, input);
 };
