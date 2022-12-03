1dd7cf4cbe9d9cbfba1c4ad55e770c48f4c3d393
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-02 19:01:21 -0700

    add linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+  },
+};
