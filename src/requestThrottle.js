116845879e0bd6739e579cd93f165a60a209dcd1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-03 22:14:48 -0700

    generate puzzle key

--- src/requestThrottle.js
+++ src/requestThrottle.js
@@ -1,3 +1,6 @@
+// create throttle.json file at root and store data in it.
+// { lastRequestTime: iso8601String, solvedPuzzles: ['puzzle hash']}
+
 /**
  * Store the Date of the last request made to the aoc servers.
  * @param {Date} date
@@ -8,6 +11,14 @@ export const setLastRequestTime = (date) => {};
  * Get the Date of the last request made to the aoc servers.
  */
 export const getLastRequestTime = () => new Date();
+
+/**
+ * Generates a key to identify the puzzle for the specific year/day/part combination.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+const generatePuzzleKey = (year, day, part) => `${year}_${day}_${part}`;
 
 /**
  * Store the fact that the puzzle has been solved.
