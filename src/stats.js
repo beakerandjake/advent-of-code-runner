d7393ba1400218547cf5a605cdd9ec326c4fb72f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-07 14:14:34 -0700

    create stats

---
+++ src/stats.js
@@ -0,0 +1,25 @@
+/**
+ * Generates a key to identify the puzzle for the specific year/day/part combination.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+const generatePuzzleKey = (year, day, part) => `${year}_${day}_${part}`;
+
+/**
+ * Store the fact that the puzzle has been solved.
+ * Prevents re-submissions of already solved puzzles.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+export const setPuzzleSolved = (year, day, part) => {};
+
+/**
+ * Has this puzzle already been solved?
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+export const hasPuzzleBeenSolved = (year, day, part) => {};
+
