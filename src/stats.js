25536593ef6638c062aa06807b6ccd0abb1ec113
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-07 15:16:37 -0700

    store solved puzzles

--- src/stats.js
+++ src/stats.js
@@ -1,10 +1,32 @@
+import { logger } from './logger.js';
+import { getStoreValue, setStoreValue } from './store.js';
+
 /**
- * Generates a key to identify the puzzle for the specific year/day/part combination.
+ * The key used in the data store where the last request time value is saved.
+ */
+const SOLVED_PUZZLES_STORE_KEY = 'solvedPuzzles';
+
+/**
+ * Generates a hash from the puzzles specific year/day/part combination.
  * @param {Number} year
  * @param {Number} day
  * @param {Number} part
  */
-const generatePuzzleKey = (year, day, part) => `${year}_${day}_${part}`;
+const generatePuzzleHash = (year, day, part) => `${year}.${day}.${part}`;
+
+/**
+ * Has this puzzle already been solved?
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+export const puzzleHasBeenSolved = async (year, day, part) => {
+  const solvedPuzzles = await getStoreValue(SOLVED_PUZZLES_STORE_KEY, []);
+  const hash = generatePuzzleHash(year, day, part);
+  const solved = solvedPuzzles.includes(hash);
+  logger.debug('solved puzzles includes year: %s, day: %s, part: %s? %s', year, day, part, solved);
+  return solved;
+};
 
 /**
  * Store the fact that the puzzle has been solved.
@@ -13,13 +35,16 @@ const generatePuzzleKey = (year, day, part) => `${year}_${day}_${part}`;
  * @param {Number} day
  * @param {Number} part
  */
-export const setPuzzleSolved = (year, day, part) => {};
+export const setPuzzleSolved = async (year, day, part) => {
+  logger.debug('marking puzzle year: %s, day: %s, part: %s as solved', year, day, part);
 
-/**
- * Has this puzzle already been solved?
- * @param {Number} year
- * @param {Number} day
- * @param {Number} part
- */
-export const hasPuzzleBeenSolved = (year, day, part) => {};
+  const solvedPuzzles = await getStoreValue(SOLVED_PUZZLES_STORE_KEY, []);
+  const hash = generatePuzzleHash(year, day, part);
 
+  if (solvedPuzzles.includes(hash)) {
+    logger.warn('attempted to set already solved puzzle: %s.%s.%s as solved', year, day, part);
+    return;
+  }
+
+  await setStoreValue(SOLVED_PUZZLES_STORE_KEY, [...solvedPuzzles, hash]);
+};
