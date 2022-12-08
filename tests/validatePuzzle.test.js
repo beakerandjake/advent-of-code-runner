b5afdfda37012a89cec8a3bd17f5152a44b2030e
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-07 22:31:08 -0700

    wip tests

--- tests/validatePuzzle.test.js
+++ tests/validatePuzzle.test.js
@@ -1,19 +1,11 @@
 import { yearIsValid, dayIsValid, partIsValid } from '../src/validatePuzzle';
 
-const nonNumericCases = [
-  null, undefined, '', Infinity, 'Cats!', false, [],
+const commonFailCases = [
+  null, undefined, '', Infinity, 'Cats!', true, [], 0, -100, Promise.resolve(1), new Error(), () => 1,
 ];
 
-test.each(nonNumericCases)('yearIsValid - false on non-numeric: %p', (value) => {
+test.each(commonFailCases)('yearIsValid - false on: %p', (value) => {
   expect(yearIsValid(value)).toBe(false);
-});
-
-test('yearIsValid - false on negative number', () => {
-  expect(yearIsValid(-1999)).toBe(false);
-});
-
-test('yearIsValid - false on zero', () => {
-  expect(yearIsValid(0)).toBe(false);
 });
 
 test('yearIsValid - false on before start year', () => {
@@ -26,21 +18,17 @@ test.todo('yearIsValid - false in this year not december');
 test.todo('yearIsValid - true between min year and last year');
 test.todo('yearIsValid - true in this year and its december');
 
-test.each(nonNumericCases)('dayIsValid - false on non-numeric: %p', (value) => {
+test.each(commonFailCases)('dayIsValid - false on: %p', (value) => {
   expect(dayIsValid(value)).toBe(false);
 });
 
-test.todo('dayIsValid - false on negative');
-test.todo('dayIsValid - false on zero');
 test.todo('dayIsValid - false on above max day');
 test.todo('dayIsValid - false on below min day');
 test.todo('dayIsValid - true when between min and max');
 
-test.each(nonNumericCases)('partIsValid - false on non-numeric: %p', (value) => {
+test.each(commonFailCases)('partIsValid - false on: %p', (value) => {
   expect(partIsValid(value)).toBe(false);
 });
 
-test.todo('partIsValid - false on negative');
-test.todo('partIsValid - false on zero');
 test.todo('partIsValid - false when below min part');
 test.todo('partIsValid - false when above max part');
