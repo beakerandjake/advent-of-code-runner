adc1913db94f3fba402628c313ff850348ac20a0
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-08 17:35:15 -0700

    wip main

--- src/main.js
+++ src/main.js
@@ -1,100 +1,35 @@
 import { Command } from 'commander';
-import { downloadInput, submitSolution } from './api.js';
-import { inputFileExits, saveInputToFile, loadInputFile } from './input.js';
-import { getConfigValue } from './config.js';
+import { exit, argv } from 'process';
+import { solveCommand } from './cli/index.js';
 import { logger } from './logger.js';
-import { solve } from './solve.js';
-import { humanizeDuration } from './formatting.js';
 
 const program = new Command();
 
 program
   .name('advent-of-code-runner')
   .description('CLI to save Christmas')
-  .version('');
+  .version('')
+  .exitOverride();
 
-program
-  .command('solve')
-  .description('Solve the puzzle and output the result to the console.')
-  .argument('<day>', 'The day of the puzzle to solve (1-24)', )
-  .option('-y, --year <number>', 'The year of advent-of-code to solve', getConfigValue('aoc.year'))
-  .action((day, options) => {
-    logger.info('hello world: %s %s', day, options);
-  });
+program.addCommand(solveCommand);
+// solveCommand.copyInheritedSettings(program);
 
-program.parse();
-
-// const year = getConfigValue('aoc.year');
-// const day = 1;
-// const part = 1;
-
-// // eslint-disable-next-line no-unused-vars
-// const main = async () => {
-//   let input;
-
-//   if (!await inputFileExits(year, day)) {
-//     // download and cache input when it doesn't exist.
-//     input = await downloadInput(year, day, getConfigValue('aoc.authenticationToken'));
-//     await saveInputToFile(year, day, input);
-//   } else {
-//     // load cached input instead of re-downloading.
-//     input = await loadInputFile(year, day);
-//   }
-
-//   const { solution, executionTimeNs } = await solve(year, day, part, input);
-
-//   logger.info('solution: %s solved in: %s', solution, humanizeDuration(executionTimeNs));
-
-//   logger.info('submission result: %s', submissionResult);
-// };
-
-// await main();
-
-// const input = await loadInputFile(year, day);
-// const { solution, executionTimeMs } = await solve(year, day, part, input);
-// logger.info('solution: %s solved in: %s', solution, humanizeDuration(executionTimeMs));
+try {
+  await program.parseAsync(argv);
+} catch (error) {
+  logger.error('%s', error);
+  exit(1);
+}
 
 // Submit Problem
 
-// Store data on local machine,
 // Hash the session token and store which problems have been solved
 // prevent re-submissions
 // store last submission time, don't allow submission if too soon
 // provide way to clear local data.
 
-// validate day / year, don't allow days in the future
-// validate day (don't select invalid day of month (1-25))
-// validate year, set minimum year
-
 // init command to scaffold solution files for an entire year
 //  create a day_x.js file for each day of that month.
 //  skip creation of files that already exist.
 
-// Allow ctrl+c to cancel a running solution (run solution on worker thread or process?)
-// swallow console logs for solutions and route to winston instead with custom
-// log level and color.
-
-// /**
-//  * Generates a key to identify the puzzle for the specific year/day/part combination.
-//  * @param {Number} year
-//  * @param {Number} day
-//  * @param {Number} part
-//  */
-// const generatePuzzleKey = (year, day, part) => `${year}_${day}_${part}`;
-
-// /**
-//  * Store the fact that the puzzle has been solved.
-//  * Prevents re-submissions of already solved puzzles.
-//  * @param {Number} year
-//  * @param {Number} day
-//  * @param {Number} part
-//  */
-// export const setPuzzleSolved = (year, day, part) => {};
-
-// /**
-//  * Has this puzzle already been solved?
-//  * @param {Number} year
-//  * @param {Number} day
-//  * @param {Number} part
-//  */
-// export const hasPuzzleBeenSolved = (year, day, part) => {};
+// route solution logs to winston with custom log level and color.
