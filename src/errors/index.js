be7d52a3286e7e0297bbfdecdb27ce0086acc151
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-10 14:57:08 -0700

    rename

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,21 +1,21 @@
 import { LockedOrCompletedPuzzleError } from './lockedOrCompletedPuzzleError.js';
 import { LockedPuzzleError } from './lockedPuzzleError.js';
 import { PuzzleAlreadySolvedError } from './puzzleAlreadySolvedError.js';
-import { SolutionFileMissingRequiredFunctionError } from './solutionFileMissingRequiredFunctionError.js';
-import { SolutionFileNotFoundError } from './solutionFileNotFoundError.js';
-import { SolutionRuntimeError } from './solutionRuntimeError.js';
 import { RateLimitExceededError } from './rateLimitExceededError.js';
+import { SolutionFileMissingRequiredFunctionError } from './solutionFileMissingRequiredFunctionError.js';
+import { SolutionNotFoundError } from './solutionNotFoundError.js';
+import { SolutionRunnerAnswerTypeError } from './solutionRunnerAnswerTypeError.js';
+import { SolutionRunnerExitError } from './solutionRunnerExitError.js';
+import { SolutionRuntimeError } from './solutionRuntimeError.js';
 import { UnexpectedSolutionRunnerWorkerError } from './unexpectedSolutionRunnerWorkerError.js';
 import { UnknownSolutionRunnerWorkerMessageTypeError } from './unknownSolutionRunnerWorkerMessageTypeError.js';
-import { SolutionRunnerExitError } from './solutionRunnerExitError.js';
-import { SolutionRunnerAnswerTypeError } from './solutionRunnerAnswerTypeError.js';
 
 export {
   LockedOrCompletedPuzzleError,
   LockedPuzzleError,
   PuzzleAlreadySolvedError,
   SolutionFileMissingRequiredFunctionError,
-  SolutionFileNotFoundError,
+  SolutionNotFoundError,
   SolutionRuntimeError,
   RateLimitExceededError,
   UnexpectedSolutionRunnerWorkerError,
--- src/errors/solutionNotFoundError.js
+++ src/errors/solutionNotFoundError.js
@@ -1,7 +1,7 @@
 /**
  * Error that is raised when a solution file for a puzzle is missing.
  */
-export class SolutionFileNotFoundError extends Error {
+export class SolutionNotFoundError extends Error {
   constructor(message) {
     super(message);
     this.name = 'SolutionFileNotFoundError';
