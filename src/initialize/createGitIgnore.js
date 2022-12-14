c4cc932e1a6a0b386c358ba546f2d61a13f43043
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-13 17:39:10 -0700

    wip init changes

---
+++ src/initialize/createGitIgnore.js
@@ -0,0 +1,11 @@
+import { copyFile } from '../io';
+import { logger } from '../logger.js';
+
+/**
+ * Creates a gitignore file from the gitignore template in the cwd.
+ */
+export const createGitIgnore = async () => {
+  logger.debug('creating .gitignore file');
+
+    // await copyFile()
+};
