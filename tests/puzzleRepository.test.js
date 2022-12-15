d89f76d03bf06151a5bbf832112b6960f785e83f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-15 11:20:31 -0700

    add tests for translate

--- tests/puzzleRepository.test.js
+++ tests/puzzleRepository.test.js
@@ -11,85 +11,84 @@ jest.unstable_mockModule('../src/user-data/jsonFileStore.js', () => ({
 const { translateToPuzzleFromData, translateToDataFromPuzzle } = await import('../src/user-data/puzzleRepository.js');
 
 describe('puzzleRepository', () => {
-  beforeEach(() => jest.resetAllMocks());
   describe('translateToPuzzleFromData()', () => {
-    // test('throws with null value', () => {
-    //   expect(() => translateToPuzzleFromData(null)).toThrow(UserDataTranslationError);
-    // });
+    test('throws with null value', () => {
+      expect(() => translateToPuzzleFromData(null)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws with undefined value', () => {
-    //   expect(() => translateToPuzzleFromData(undefined)).toThrow(UserDataTranslationError);
-    // });
+    test('throws with undefined value', () => {
+      expect(() => translateToPuzzleFromData(undefined)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws with non object', () => {
-    //   expect(() => translateToPuzzleFromData(1234)).toThrow(UserDataTranslationError);
-    //   expect(() => translateToPuzzleFromData(() => {})).toThrow(UserDataTranslationError);
-    //   expect(
-    //     () => translateToPuzzleFromData(Promise.resolve(10)),
-    //   ).toThrow(UserDataTranslationError);
-    //   expect(() => translateToPuzzleFromData('asdf')).toThrow(UserDataTranslationError);
-    // });
+    test('throws with non object', () => {
+      expect(() => translateToPuzzleFromData(1234)).toThrow(UserDataTranslationError);
+      expect(() => translateToPuzzleFromData(() => {})).toThrow(UserDataTranslationError);
+      expect(
+        () => translateToPuzzleFromData(Promise.resolve(10)),
+      ).toThrow(UserDataTranslationError);
+      expect(() => translateToPuzzleFromData('asdf')).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws missing id', () => {
-    //   const puzzle = {
-    //     fastestExecutionTimeNs: null,
-    //     incorrectAnswers: [],
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws missing id', () => {
+      const puzzle = {
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws non string id', () => {
-    //   const puzzle = {
-    //     id: 20021205,
-    //     fastestExecutionTimeNs: null,
-    //     incorrectAnswers: [],
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws non string id', () => {
+      const puzzle = {
+        id: 20021205,
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws id contains non-numeric character', () => {
-    //   const puzzle = {
-    //     id: '200212z5',
-    //     fastestExecutionTimeNs: null,
-    //     incorrectAnswers: [],
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws id contains non-numeric character', () => {
+      const puzzle = {
+        id: '200212z5',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws with id invalid length', () => {
-    //   const puzzle = {
-    //     id: '202212',
-    //     fastestExecutionTimeNs: null,
-    //     incorrectAnswers: [],
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws with id invalid length', () => {
+      const puzzle = {
+        id: '202212',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws with incorrectAnswers not array', () => {
-    //   const puzzle = {
-    //     id: '20221201',
-    //     fastestExecutionTimeNs: null,
-    //     incorrectAnswers: {},
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws with incorrectAnswers not array', () => {
+      const puzzle = {
+        id: '20221201',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: {},
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    // test('throws with fastestExecutionTime not number', () => {
-    //   const puzzle = {
-    //     id: '20221201',
-    //     fastestExecutionTimeNs: 'Really Fast!',
-    //     incorrectAnswers: [],
-    //     correctAnswer: null,
-    //   };
-    //   expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
-    // });
+    test('throws with fastestExecutionTime not number', () => {
+      const puzzle = {
+        id: '20221201',
+        fastestExecutionTimeNs: 'Really Fast!',
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+      expect(() => translateToPuzzleFromData(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-    test('correctly translates year, day, part from id', () => {
+    test('translates year, day, part from id', () => {
       const data = {
         id: '20220201',
         fastestExecutionTimeNs: null,
@@ -106,7 +105,20 @@ describe('puzzleRepository', () => {
       expect(translateToPuzzleFromData(data)).toMatchObject(expected);
     });
 
-    test('correctly translates fastestExecutionTimeNs', () => {
+    test('translates id', () => {
+      const data = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers: [],
+        correctAnswer: null,
+      };
+
+      const expected = { id: '20220201' };
+
+      expect(translateToPuzzleFromData(data)).toMatchObject(expected);
+    });
+
+    test('translates fastestExecutionTimeNs', () => {
       const data = {
         id: '20220201',
         fastestExecutionTimeNs: 12345,
@@ -120,122 +132,203 @@ describe('puzzleRepository', () => {
 
       expect(translateToPuzzleFromData(data)).toMatchObject(expected);
     });
+
+    test('translates incorrectAnswers', () => {
+      const incorrectAnswers = ['asdf', 'qwerty', 'zaps'];
+
+      const data = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers,
+        correctAnswer: null,
+      };
+
+      const expected = { incorrectAnswers };
+
+      expect(translateToPuzzleFromData(data)).toMatchObject(expected);
+    });
+
+    test('translates correctAnswer', () => {
+      const data = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers: [],
+        correctAnswer: 'asdf',
+      };
+
+      const expected = { correctAnswer: 'asdf' };
+
+      expect(translateToPuzzleFromData(data)).toMatchObject(expected);
+    });
   });
 
-  // describe('translateToDataFromPuzzle()', () => {
-  //   test('throws with null value', () => {
-  //     expect(() => translateToDataFromPuzzle(null)).toThrow(UserDataTranslationError);
-  //   });
+  describe('translateToDataFromPuzzle()', () => {
+    test('throws with null value', () => {
+      expect(() => translateToDataFromPuzzle(null)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws with undefined value', () => {
-  //     expect(() => translateToDataFromPuzzle(undefined)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws with undefined value', () => {
+      expect(() => translateToDataFromPuzzle(undefined)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws with non object', () => {
-  //     expect(() => translateToDataFromPuzzle(1234)).toThrow(UserDataTranslationError);
-  //     expect(() => translateToDataFromPuzzle(() => {})).toThrow(UserDataTranslationError);
-  //     expect(
-  //       () => translateToDataFromPuzzle(Promise.resolve(10)),
-  //     ).toThrow(UserDataTranslationError);
-  //     expect(() => translateToDataFromPuzzle('asdf')).toThrow(UserDataTranslationError);
-  //   });
+    test('throws with non object', () => {
+      expect(() => translateToDataFromPuzzle(1234)).toThrow(UserDataTranslationError);
+      expect(() => translateToDataFromPuzzle(() => {})).toThrow(UserDataTranslationError);
+      expect(
+        () => translateToDataFromPuzzle(Promise.resolve(10)),
+      ).toThrow(UserDataTranslationError);
+      expect(() => translateToDataFromPuzzle('asdf')).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws missing id', () => {
-  //     const puzzle = {
-  //       fastestExecutionTimeNs: null,
-  //       incorrectAnswers: [],
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws missing id', () => {
+      const puzzle = {
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws non string id', () => {
-  //     const puzzle = {
-  //       id: 20021205,
-  //       fastestExecutionTimeNs: null,
-  //       incorrectAnswers: [],
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws non string id', () => {
+      const puzzle = {
+        id: 20021205,
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws id contains non-numeric character', () => {
-  //     const puzzle = {
-  //       id: 'J0021225',
-  //       fastestExecutionTimeNs: null,
-  //       incorrectAnswers: [],
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws id contains non-numeric character', () => {
+      const puzzle = {
+        id: 'J0021225',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws with id invalid length', () => {
-  //     const puzzle = {
-  //       id: '202212',
-  //       fastestExecutionTimeNs: null,
-  //       incorrectAnswers: [],
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws with id invalid length', () => {
+      const puzzle = {
+        id: '202212',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: [],
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws with incorrectAnswers not array', () => {
-  //     const puzzle = {
-  //       id: '20221201',
-  //       fastestExecutionTimeNs: null,
-  //       incorrectAnswers: {},
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws with incorrectAnswers not array', () => {
+      const puzzle = {
+        id: '20221201',
+        fastestExecutionTimeNs: null,
+        incorrectAnswers: {},
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('throws with fastestExecutionTime not number', () => {
-  //     const puzzle = {
-  //       id: '20221201',
-  //       fastestExecutionTimeNs: 'Really Fast!',
-  //       incorrectAnswers: [],
-  //       correctAnswer: null,
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
-  //     expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
-  //   });
+    test('throws with fastestExecutionTime not number', () => {
+      const puzzle = {
+        id: '20221201',
+        fastestExecutionTimeNs: 'Really Fast!',
+        incorrectAnswers: [],
+        correctAnswer: null,
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
+      expect(() => translateToDataFromPuzzle(puzzle)).toThrow(UserDataTranslationError);
+    });
 
-  //   test('translates to expected', () => {
-  //     const data = {
-  //       id: '20220201',
-  //       fastestExecutionTimeNs: 2343455,
-  //       incorrectAnswers: ['wrong', 'answers', 'only'],
-  //       correctAnswer: 'asdf',
-  //       year: 2022,
-  //       day: 20,
-  //       part: 1,
-  //     };
+    test('strips properties (year, day, part)', () => {
+      const data = {
+        id: '20220201',
+        fastestExecutionTimeNs: 2343455,
+        incorrectAnswers: ['wrong', 'answers', 'only'],
+        correctAnswer: 'asdf',
+        year: 2022,
+        day: 20,
+        part: 1,
+      };
 
-  //     const expected = {
-  //       id: '20220201',
-  //       fastestExecutionTimeNs: 2343455,
-  //       incorrectAnswers: ['wrong', 'answers', 'only'],
-  //       correctAnswer: 'asdf',
-  //     };
+      const translated = translateToDataFromPuzzle(data);
 
-  //     expect(translateToDataFromPuzzle(data)).toMatchObject(expected);
-  //   });
-  // });
+      expect(translated).not.toHaveProperty('year');
+      expect(translated).not.toHaveProperty('day');
+      expect(translated).not.toHaveProperty('part');
+    });
+
+    test('translates id', () => {
+      const puzzle = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers: ['asdf'],
+        correctAnswer: 'qwer',
+      };
+
+      const expected = { id: '20220201' };
+
+      expect(translateToDataFromPuzzle(puzzle)).toMatchObject(expected);
+    });
+
+    test('translates fastestExecutionTimeNs', () => {
+      const puzzle = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers: ['asdf'],
+        correctAnswer: 'qwer',
+      };
+
+      const expected = { fastestExecutionTimeNs: 12345 };
+
+      expect(translateToDataFromPuzzle(puzzle)).toMatchObject(expected);
+    });
+
+    test('translates incorrectAnswers', () => {
+      const incorrectAnswers = ['asdf', 'qwerty', 'zaps'];
+
+      const puzzle = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers,
+        correctAnswer: 'qwer',
+      };
+
+      const expected = { incorrectAnswers };
+
+      expect(translateToDataFromPuzzle(puzzle)).toMatchObject(expected);
+    });
+
+    test('translates correctAnswer', () => {
+      const correctAnswer = '1234';
+
+      const puzzle = {
+        id: '20220201',
+        fastestExecutionTimeNs: 12345,
+        incorrectAnswers: ['asdf'],
+        correctAnswer,
+      };
+
+      const expected = { correctAnswer };
+
+      expect(translateToDataFromPuzzle(puzzle)).toMatchObject(expected);
+    });
+  });
 });
