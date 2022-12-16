c6b015a4039e17556c37535e99412e10e911bd78
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 10:12:11 -0700

    add errors

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,3 +1,5 @@
+import { AnswerTypeInvalidError } from './answerTypeInvalidError.js';
+import { AnswerEmptyError } from './answerEmptyError.js';
 import { DataFileNotFoundError } from './dataFileNotFoundError.js';
 import { LockedOrCompletedPuzzleError } from './lockedOrCompletedPuzzleError.js';
 import { LockedPuzzleError } from './lockedPuzzleError.js';
@@ -13,6 +15,8 @@ import { UserPuzzleDataMissingError } from './userPuzzleDataMissingError.js';
 import { UserError } from './userError.js';
 
 export {
+  AnswerTypeInvalidError,
+  AnswerEmptyError,
   DataFileNotFoundError,
   LockedOrCompletedPuzzleError,
   LockedPuzzleError,
