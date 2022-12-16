6922c90c1ee4a574dfdca61553e65654a44e1413
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 13:39:37 -0700

    update error message

--- src/errors/lockedOrCompletedPuzzleError.js
+++ src/errors/lockedOrCompletedPuzzleError.js
@@ -5,8 +5,8 @@ import { UserError } from './userError.js';
  * which is locked to them, or is one they have already solved
  */
 export class LockedOrCompletedPuzzleError extends UserError {
-  constructor(message) {
-    super(message);
+  constructor() {
+    super('This puzzle is locked! Either you have not completed the requisite part, or the puzzle has not been released yet!');
     this.name = 'LockedOrCompletedPuzzleError';
   }
 }
