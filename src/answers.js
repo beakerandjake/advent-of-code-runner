5869c8b4a9e4b8d91a852cd07bb9c0b009cb6aaa
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 10:26:08 -0700

    wip testing answers

--- src/answers.js
+++ src/answers.js
@@ -1,19 +1,59 @@
+import { AnswerTypeInvalidError, AnswerEmptyError } from './errors/index.js';
+import { logger } from './logger.js';
+import { addOrEditPuzzle, createPuzzle, findPuzzle } from './persistence/puzzleRepository.js';
+
+/**
+ * Validates and normalizes a user provided answer.
+ * Ensures answer is non empty and is a number or a string.
+ * @param {String|Number} answer
+ * @throws {AnswerEmptyError}
+ * @throws {AnswerTypeInvalidError}
+ */
+export const parseAnswer = (answer) => {
+  if (answer === null || answer === undefined) {
+    throw new AnswerEmptyError();
+  }
+
+  // only supported type is number or string.
+  if (typeof answer !== 'string' && typeof answer !== 'number') {
+    throw new AnswerTypeInvalidError(typeof answer);
+  }
+
+  const trimmedAnswer = answer.toString().trim();
+
+  // don't allow empty strings.
+  if (!trimmedAnswer.length) {
+    throw new AnswerEmptyError();
+  }
+
+  return trimmedAnswer;
+};
+
 /**
  * Has this puzzle already been solved?
  * @param {Number} year
  * @param {Number} day
  * @param {Number} part
  */
-export const puzzleHasBeenSolved = async (year, day, part) => null;
+export const puzzleHasBeenSolved = async (year, day, part) => {
+  const solved = !!(await findPuzzle(year, day, part))?.correctAnswer;
+  logger.debug('puzzle is solved: %s', solved, { year, day, part });
+  return !!solved;
+};
 
 /**
- * Returns the correct answer for this puzzle (if solved, null if not).
+ * Returns the correct answer for this puzzle
+ * Returns null if the puzzle has not been solved yet.
  * @param {Number} year
  * @param {Number} day
  * @param {Number} part
  * @returns {Promise<String>}
  */
-export const getCorrectAnswer = async (year, day, part) => null;
+export const getCorrectAnswer = async (year, day, part) => {
+  const { correctAnswer = null } = await findPuzzle(year, day, part) || {};
+  logger.debug('got correct answer for puzzle: %s', correctAnswer, { year, day, part });
+  return correctAnswer;
+};
 
 /**
  * Stores the correct answer for this puzzle.
@@ -22,7 +62,23 @@ export const getCorrectAnswer = async (year, day, part) => null;
  * @param {Number} part
  * @param {String} correctAnswer
  */
-export const setCorrectAnswer = async (year, day, part, correctAnswer) => null;
+export const setCorrectAnswer = async (year, day, part, correctAnswer) => {
+  // Only support string or numer
+  if (correctAnswer == null || typeof correctAnswer !== 'string' || typeof correctAnswer !== 'number') {
+    throw new AnswerTypeInvalidError(typeof correctAnswer);
+  }
+
+  const trimmedAnswer = correctAnswer.toString().trim();
+
+  if (!trimmedAnswer) {
+    throw new AnswerEmptyError();
+  }
+
+  logger.debug('saving correct answer: "%s"', correctAnswer, { year, day, part });
+  const puzzle = await findPuzzle(year, day, part) || createPuzzle(year, day, part);
+  const updatedPuzzle = { ...puzzle, correctAnswer: trimmedAnswer };
+  await addOrEditPuzzle(updatedPuzzle);
+};
 
 /**
  * Stores an incorrect answer for this puzzle.
---
+++ tests/answers.test.js
@@ -0,0 +1,104 @@
+import { describe, jest, test } from '@jest/globals';
+import { AnswerTypeInvalidError, AnswerEmptyError } from '../src/errors/index.js';
+
+// setup mocks.
+jest.unstable_mockModule('../src/persistence/puzzleRepository.js', () => ({
+  findPuzzle: jest.fn(),
+  createPuzzle: jest.fn(),
+  addOrEditPuzzle: jest.fn(),
+}));
+
+jest.unstable_mockModule('../src/logger.js', () => ({
+  logger: {
+    debug: jest.fn(),
+  },
+}));
+
+// import after setting up the mock so the modules import the mocked version
+const { findPuzzle, createPuzzle, addOrEditPuzzle } = await import('../src/persistence/puzzleRepository.js');
+const {
+  puzzleHasBeenSolved, getCorrectAnswer, setCorrectAnswer, parseAnswer,
+} = await import('../src/answers.js');
+
+describe('answers', () => {
+  describe('puzzleHasBeenSolved()', () => {
+    test('returns false if puzzle not found', async () => {
+      findPuzzle.mockReturnValueOnce(null);
+      expect(await puzzleHasBeenSolved(2022, 1, 1)).toBe(false);
+    });
+
+    test('returns true if puzzle has correctAnswer set', async () => {
+      findPuzzle.mockReturnValueOnce({
+        correctAnswer: 'bobcat',
+      });
+      expect(await puzzleHasBeenSolved(2022, 12, 1)).toBe(true);
+    });
+
+    test('returns false if puzzle does not have correctAnswer set', async () => {
+      findPuzzle.mockReturnValueOnce({
+        correctAnswer: null,
+      });
+      expect(await puzzleHasBeenSolved(2022, 12, 1)).toBe(false);
+    });
+  });
+
+  describe('getCorrectAnswer()', () => {
+    test('returns null if puzzle not found', async () => {
+      findPuzzle.mockReturnValueOnce(null);
+      expect(await getCorrectAnswer(2022, 1, 1)).toBe(null);
+    });
+
+    test('returns correctAnswer if puzzle found', async () => {
+      const correctAnswer = 'ASDF';
+      findPuzzle.mockReturnValueOnce({ correctAnswer });
+      expect(await getCorrectAnswer(2022, 1, 1)).toBe(correctAnswer);
+    });
+  });
+
+  describe('parseAnswer()', () => {
+    test('throws on null', () => {
+      expect(() => parseAnswer(null)).toThrow(AnswerEmptyError);
+    });
+
+    test('throws on undefined', () => {
+      expect(() => parseAnswer(null)).toThrow(AnswerEmptyError);
+    });
+
+    test('throws on invalid type', () => {
+      expect(() => parseAnswer([])).toThrow(AnswerTypeInvalidError);
+      expect(() => parseAnswer(false)).toThrow(AnswerTypeInvalidError);
+      expect(() => parseAnswer({})).toThrow(AnswerTypeInvalidError);
+      expect(() => parseAnswer(Promise.resolve('ASDF'))).toThrow(AnswerTypeInvalidError);
+      expect(() => parseAnswer(() => {})).toThrow(AnswerTypeInvalidError);
+      expect(() => parseAnswer(new Error('WRONG'))).toThrow(AnswerTypeInvalidError);
+    });
+
+    test('throws on empty string', () => {
+      expect(() => parseAnswer('')).toThrow(AnswerEmptyError);
+    });
+
+    test('throws on whitespace', () => {
+      console.log('typeof', typeof '\t\t\t\t\t');
+      expect(() => parseAnswer('\t\t\t\t\t')).toThrow(AnswerEmptyError);
+      expect(() => parseAnswer('\r\n')).toThrow(AnswerEmptyError);
+      expect(() => parseAnswer('     ')).toThrow(AnswerEmptyError);
+    });
+
+    test('trims a string answer', () => {
+      const answer = '\tASDF\t   ';
+      const trimmed = answer.trim();
+      expect(parseAnswer(answer)).toEqual(trimmed);
+    });
+
+    test('accepts a string answer', () => {
+      const answer = 'ASDF';
+      expect(parseAnswer(answer)).toEqual(answer);
+    });
+
+    test('accepts a number answer', () => {
+      const answer = 123412341234;
+      const expected = answer.toString();
+      expect(parseAnswer(answer)).toEqual(expected);
+    });
+  });
+});
