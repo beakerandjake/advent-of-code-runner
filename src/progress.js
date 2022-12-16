376c4d3890db2e4a982a725bbc18dca8efa098f9
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 13:40:14 -0700

    remove all but todo note

--- src/progress.js
+++ src/progress.js
@@ -32,9 +32,8 @@
       -getMostFailedAttemptsCount.js
       -index.js - exports all functions as named or exports into named statsService object?
 
-    progress/ 
+    progress/
       getNextUnansweredPuzzle.js
-      
 
     api/
       ...
@@ -45,40 +44,3 @@
  *
  *
  */
-
-/**
- * Attempt to update the fastest execution time for this puzzle.
- * The fastest time will only be updated if the puzzle has a correct answer set
- * and the provided execution time is smaller than the current fastest time.
- * @param {Number} year
- * @param {Number} day
- * @param {Number} part
- * @param {Number} executionTimeNs
- */
-export const tryToSetFastestExecutionTime = async (year, day, part, executionTimeNs) => {
-  logger.debug('trying to set fastest execution time for year: %s, day: %s, part: %s', year, day, part);
-
-  if (!Number.isFinite(executionTimeNs)) {
-    throw new Error('Attempted to set fastest execution time to non numeric value');
-  }
-
-  const puzzles = await getPuzzles();
-  const puzzle = findPuzzle(puzzleId(year, day, part), puzzles);
-
-  // bail if puzzle has not been solved
-  if (!puzzle?.correctAnswer) {
-    logger.debug('not setting fastest execution time, puzzle has not been successfully answered.');
-    return;
-  }
-
-  // bail if execution time was too slow
-  if (puzzle.fastestExecutionTimeNs <= executionTimeNs) {
-    logger.debug('not setting fastest execution time, execution time: %s was slower than stored fastest: %s', executionTimeNs, puzzle.fastestExecutionTimeNs);
-    return;
-  }
-
-  logger.festive('That\'s your fastest execution time ever for this puzzle!');
-  const changes = { ...puzzle, fastestExecutionTimeNs: executionTimeNs };
-  await setPuzzles(addOrUpdatePuzzle(changes, puzzles));
-};
-
