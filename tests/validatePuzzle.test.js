0137f546e08b8aef24ea9d97369dcc8199f0494a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 12:09:02 -0700

    getAllPuzzles

--- tests/validatePuzzle.test.js
+++ tests/validatePuzzle.test.js
@@ -13,7 +13,9 @@ jest.unstable_mockModule('../src/logger.js', () => ({
 
 // import after setting up the mock so the modules import the mocked version
 const { getConfigValue } = await import('../src/config.js');
-const { yearIsValid, dayIsValid, partIsValid } = await import('../src/validatePuzzle.js');
+const {
+  yearIsValid, dayIsValid, partIsValid, getAllPuzzles,
+} = await import('../src/validatePuzzle.js');
 
 // since some tests override the mock implementation, we have to reset it before each test.
 beforeEach(() => getConfigValue.mockImplementation(originalGetConfigValue));
@@ -161,6 +163,33 @@ describe('validatePuzzle', () => {
         ['aoc.puzzleValidation.parts', [6, 7, 8]],
       ]);
       expect(partIsValid(8)).toBe(true);
+    });
+  });
+
+  describe('getAllPuzzles()', () => {
+    test('returns expected value', () => {
+      const year = 2022;
+      getConfigValue.mockReturnValueOnce([1, 2, 3, 4, 5]);
+      getConfigValue.mockReturnValueOnce([1, 2, 3]);
+      const expected = [
+        { year, day: 1, part: 1 },
+        { year, day: 1, part: 2 },
+        { year, day: 1, part: 3 },
+        { year, day: 2, part: 1 },
+        { year, day: 2, part: 2 },
+        { year, day: 2, part: 3 },
+        { year, day: 3, part: 1 },
+        { year, day: 3, part: 2 },
+        { year, day: 3, part: 3 },
+        { year, day: 4, part: 1 },
+        { year, day: 4, part: 2 },
+        { year, day: 4, part: 3 },
+        { year, day: 5, part: 1 },
+        { year, day: 5, part: 2 },
+        { year, day: 5, part: 3 },
+      ];
+
+      expect(getAllPuzzles(year)).toStrictEqual(expected);
     });
   });
 });
