f1729cfc7c7d7f08614d8b6a0d07e2894b17adbe
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-17 09:02:47 -0700

    update todo

--- src/todo.js
+++ src/todo.js
@@ -3,7 +3,7 @@
   src/
 
     validation/
-      userArgs.js (could rename to  puzzle? add getAllPuzzlesForYear? )
+      userArgs.js (could rename to  puzzle? add getAllPuzzlesForYear? add isUnlocked? )
         yearIsValid
         dayIsValid
         partIsValid
@@ -12,12 +12,20 @@
         answerTypeIsValid - string or number
         answerIsValid -> is valid type and non empty.
         normalizeAnswer -> trim and convert to string.
-        
-      puzzle.js
+
+      puzzle.js (could move to puzzle.js if rename userArgs)
         isUnlocked
+
+      solutions
+        solve.js
+        solutionRunner
+        solutionRunnerWorkerThread
 
       where to put getAllPuzzlesForYear?? need a better name
 
+  cli specifies args and such but calls separate action functions that's easier tested
+  break out function in solutionRunnerWorkerThread & export for testing.
+  need to test api, pull out response parsing into own files?
  *
  *
  */
