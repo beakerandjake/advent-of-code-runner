0b5f8c10501123839f2a0e8e5018e6d2077e666e
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-16 22:59:49 -0700

    keeping file for a bit.

---
+++ src/todo.js
@@ -0,0 +1,23 @@
+/**
+
+  src/
+
+    validation/
+      userArgs.js (could rename to  puzzle? add getAllPuzzlesForYear? )
+        yearIsValid
+        dayIsValid
+        partIsValid
+
+      userAnswer.js (type)
+        answerTypeIsValid - string or number
+        answerIsValid -> is valid type and non empty.
+        normalizeAnswer -> trim and convert to string.
+        
+      puzzle.js
+        isUnlocked
+
+      where to put getAllPuzzlesForYear?? need a better name
+
+ *
+ *
+ */
