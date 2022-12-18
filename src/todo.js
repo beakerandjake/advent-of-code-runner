4e27a0144316927ce3787570bd5ff06a13f001e9
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-17 20:01:03 -0700

    add todo

--- src/todo.js
+++ src/todo.js
@@ -21,13 +21,17 @@
         solutionRunner
         solutionRunnerWorkerThread
 
+      inputs/
+        inputCache.js
+        getInput.js
+
       where to put getAllPuzzlesForYear?? need a better name
 
   cli specifies args and such but calls separate action functions that's easier tested
   break out function in solutionRunnerWorkerThread & export for testing.
   need to test api, pull out response parsing into own files?
 
-  need one interface for input that downloads and caches if not found.. 
+  need one interface for input that downloads and caches if not found..
 
   testing... when mocking async fns, am i returning async fn/promise???
  *
