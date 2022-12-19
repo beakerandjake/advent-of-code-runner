93a5e7d0b9e33e9f54fea3df6888a65dec2ff588
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-19 15:54:31 -0700

    fix import

--- src/todo.js
+++ src/todo.js
@@ -1,23 +1,5 @@
 /**
 
-  src/
-
-    validation/
-      userArgs.js (could rename to  puzzle? add getAllPuzzlesForYear? add isUnlocked? )
-        yearIsValid
-        dayIsValid
-        partIsValid
-
-      userAnswer.js (type)
-        answerTypeIsValid - string or number
-        answerIsValid -> is valid type and non empty.
-        normalizeAnswer -> trim and convert to string.
-
-      puzzle.js (could move to puzzle.js if rename userArgs)
-        isUnlocked
-
-      where to put getAllPuzzlesForYear?? need a better name
-
   cli specifies args and such but calls separate action functions that's easier tested
   break out function in solutionRunnerWorkerThread & export for testing.
   need to test api, pull out response parsing into own files?
@@ -28,8 +10,6 @@
     commands can use one or more actions.
 
   testing... when mocking async fns, am i returning async fn/promise???
-
-  rename config options for validation
  *
  *
  */
