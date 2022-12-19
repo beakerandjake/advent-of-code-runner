df290aa858df61c5bcc50a5cd941c55d02bd779f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-18 20:50:16 -0700

    testing

--- tests/solultions/solutionRunnerWorkerThread.test.js
+++ tests/solultions/solutionRunnerWorkerThread.test.js
@@ -1,6 +1,7 @@
 import {
   beforeEach, describe, jest, test,
 } from '@jest/globals';
+import { workerMessageTypes } from '../../src/solutions/workerMessageTypes';
 
 // setup mocks
 jest.unstable_mockModule('node:worker_threads', () => ({
@@ -26,9 +27,39 @@ beforeEach(() => {
 describe('solutionRunnerWorkerThread', () => {
   describe('logFromWorker()', () => {
     test('calls parentPort.postMessage()', async () => {
-      // const args = [];
       logFromWorker('cats', 'dogs', 1234, 45, 6);
       expect(parentPort.postMessage).toHaveBeenCalledTimes(1);
+    });
+
+    test('posts expected data (with args)', async () => {
+      // const args = [];
+      const args = [1234, { name: 'bob' }, 'SNAP'];
+      const message = 'dogs';
+      const expected = {
+        type: workerMessageTypes.log,
+        level: 'cats',
+        message: `worker - ${message}`,
+        meta: [...args],
+      };
+      logFromWorker(expected.level, message, ...args);
+
+      expect(parentPort.postMessage).toHaveBeenCalledTimes(1);
+      expect(parentPort.postMessage).toHaveBeenCalledWith(expected);
+    });
+
+    test('posts expected data (without args)', async () => {
+      // const args = [];
+      const message = 'dogs';
+      const expected = {
+        type: workerMessageTypes.log,
+        level: 'cats',
+        message: `worker - ${message}`,
+        meta: [],
+      };
+      logFromWorker(expected.level, message);
+
+      expect(parentPort.postMessage).toHaveBeenCalledTimes(1);
+      expect(parentPort.postMessage).toHaveBeenCalledWith(expected);
     });
   });
 });
