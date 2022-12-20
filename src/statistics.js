624d2bd0ff0dded8f919341a2deeb86b4636d79c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 12:46:51 -0700

    remove old tests

--- src/statistics.js
+++ src/statistics.js
@@ -1,4 +1,3 @@
-import { logger } from './logger.js';
 import { addOrEditPuzzle, createPuzzle, findPuzzle } from './persistence/puzzleRepository.js';
 import { parsePositiveInt } from './validation/validationUtils.js';
 
@@ -26,33 +25,4 @@ export const setFastestExecutionTime = async (year, day, part, timeNs) => {
   const puzzle = await findPuzzle(year, day, part) || createPuzzle(year, day, part);
   const updated = { ...puzzle, fastestExecutionTimeNs: parsed };
   return addOrEditPuzzle(updated);
-};
-
-/**
- *
- * @param {Number} year
- * @param {Number} day
- * @param {Number} part
- * @param {Number} timeNs
- */
-export const tryToSetFastestExecutionTime = async (year, day, part, timeNs) => {
-  if (!Number.isFinite(timeNs) || timeNs <= 0) {
-    throw new Error('Attempted to set fastest execution time to non numeric or negative value');
-  }
-
-  const puzzle = await findPuzzle(year, day, part);
-
-  if (!puzzle) {
-    logger.warn('attempted to set fasted execution time on puzzle which was not saved');
-    return;
-  }
-
-  if (puzzle.fastestExecutionTimeNs && (timeNs >= puzzle.fastestExecutionTimeNs)) {
-    logger.debug('not setting fastest execution time, execution time: %s was slower than stored fastest: %s', timeNs, puzzle.fastestExecutionTimeNs);
-    return;
-  }
-
-  logger.festive('That\'s your fastest execution time ever for this puzzle!');
-  const updates = { ...puzzle, fastestExecutionTimeNs: timeNs };
-  await addOrEditPuzzle(updates);
 };
--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -3,9 +3,6 @@ import {
 } from '@jest/globals';
 import { mockLogger } from './mocks.js';
 
-// import { logger } from './logger.js';
-// import { addOrEditPuzzle, findPuzzle } from './persistence/puzzleRepository.js';
-
 // setup mocks.
 mockLogger();
 
@@ -22,75 +19,13 @@ jest.unstable_mockModule('src/validation/validationUtils.js', () => ({
 // import after setting up the mock so the modules import the mocked version
 const { addOrEditPuzzle, findPuzzle, createPuzzle } = await import('../src/persistence/puzzleRepository.js');
 const { parsePositiveInt } = await import('../src/validation/validationUtils.js');
-const { tryToSetFastestExecutionTime, getFastestExecutionTime, setFastestExecutionTime } = await import('../src/statistics.js');
+const { getFastestExecutionTime, setFastestExecutionTime } = await import('../src/statistics.js');
 
 afterEach(() => {
   jest.resetAllMocks();
 });
 
 describe('statistics', () => {
-  describe('tryToSetFastestExecutionTime()', () => {
-    test('throws if time is null', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, null); })
-        .rejects
-        .toThrow();
-    });
-
-    test('throws if time is undefined', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, undefined); })
-        .rejects
-        .toThrow();
-    });
-
-    test('throws if time is non numeric', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, false); })
-        .rejects
-        .toThrow();
-    });
-
-    test('throws if time is NaN', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, Infinity); })
-        .rejects
-        .toThrow();
-    });
-
-    test('throws if time is zero', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, 0); })
-        .rejects
-        .toThrow();
-    });
-
-    test('throws if time is negative', async () => {
-      expect(async () => { await tryToSetFastestExecutionTime(2022, 1, 1, -1234); })
-        .rejects
-        .toThrow();
-    });
-
-    test('noop on puzzle not found', async () => {
-      findPuzzle.mockReturnValueOnce(null);
-      await tryToSetFastestExecutionTime(2022, 1, 1, 12341234);
-      expect(addOrEditPuzzle).not.toBeCalled();
-    });
-
-    test('doest not update if time is not faster than record', async () => {
-      findPuzzle.mockReturnValueOnce({ fastestExecutionTimeNs: 55 });
-      await tryToSetFastestExecutionTime(2022, 1, 1, 12341234);
-      expect(addOrEditPuzzle).not.toBeCalled();
-    });
-
-    test('updates if time is faster than record', async () => {
-      findPuzzle.mockReturnValueOnce({ fastestExecutionTimeNs: 1234231432314 });
-      await tryToSetFastestExecutionTime(2022, 1, 1, 22);
-      expect(addOrEditPuzzle).toBeCalled();
-    });
-
-    test('updates if record time is null', async () => {
-      findPuzzle.mockReturnValueOnce({ fastestExecutionTimeNs: null });
-      await tryToSetFastestExecutionTime(2022, 1, 1, 22);
-      expect(addOrEditPuzzle).toBeCalled();
-    });
-  });
-
   describe('getFastestExecutionTime()', () => {
     test('returns null if puzzle not found', async () => {
       findPuzzle.mockResolvedValue(null);
@@ -151,9 +86,4 @@ describe('statistics', () => {
       expect(addOrEditPuzzle).toHaveBeenCalledWith({ fastestExecutionTimeNs: time });
     });
   });
-
-  // test('throws if value is not positive int', async () => {
-  //   parsePositiveInt.mockReturnValue(false);
-  //   await expect(async () => getFastestExecutionTime(2022, 1, 1)).rejects.toThrow();
-  // });
 });
