96224c1454ecd37ada98b06e55093038d347289c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 12:32:36 -0700

    first pass sovle

--- src/actions/solvePuzzleAndSubmitAnswer.js
+++ src/actions/solvePuzzleAndSubmitAnswer.js
@@ -1,4 +1,36 @@
+import { addIncorrectAnswer, setCorrectAnswer } from '../answers';
+import { logger } from '../logger.js';
+import { setFastestExecutionTime } from '../statistics';
+import { getYear, puzzleIsUnlocked } from './actionUtil.js';
+import { getInputAndExecuteSolution } from './getInputAndExecuteSolution.js';
+import { tryToSubmitPuzzleAnswer } from './tryToSubmitPuzzleAnswer.js';
 
+/**
+ * Store the fact that the submitted answer was correct.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ * @param {Object} executionResult
+ */
+const onAnswerCorrect = async (year, day, part, message, { answer, executionTimeNs }) => {
+  logger.festive('%s', message);
+  return Promise.all([
+    setCorrectAnswer(year, day, part, answer),
+    setFastestExecutionTime(year, day, part, executionTimeNs),
+  ]);
+};
+
+/**
+ * Store the fact that the submitted answer was incorrect.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ * @param {Object} executionResult
+ */
+const onAnswerIncorrect = async (year, day, part, message, { answer }) => {
+  logger.error('%s', message);
+  return addIncorrectAnswer(year, day, part, answer);
+};
 
 /**
  * Submit the answer for the specified puzzle.
@@ -6,5 +38,19 @@
  * @param {Number} part
  */
 export const solvePuzzleAndSubmitAnswer = async (day, part) => {
+  const year = getYear();
 
+  // can't submit a locked puzzle
+  if (await puzzleIsUnlocked(year, day, part) === false) {
+    return;
+  }
+
+  const { answer, executionTimeNs } = await getInputAndExecuteSolution(day, part);
+  const { success, message } = await tryToSubmitPuzzleAnswer(year, day, part, answer);
+
+  await (
+    success
+      ? onAnswerCorrect(year, day, part, message, { answer, executionTimeNs })
+      : onAnswerIncorrect(year, day, part, message, { answer, executionTimeNs })
+  );
 };
