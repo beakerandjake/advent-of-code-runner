e26c3c5685f3e82e9361aed7ec685ce56e422da8
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 15:05:22 -0700

    update todo

--- src/todo.js
+++ src/todo.js
@@ -1,19 +1,4 @@
 /**
-
-  cli specifies args and such but calls separate action functions that's easier tested
-  break out function in solutionRunnerWorkerThread & export for testing.
-  need to test api, pull out response parsing into own files?
-
-  actions vs commands
-    commands are responsible for cli taking input / validation and invoking actions
-    actions are responsible for coordinating multiple modules to achieve action
-    commands can use one or more actions.
-
   testing... when mocking async fns, am i returning async fn/promise???
 
-  commands
-    solve - test
-    init - add action and test.
- *
- *
  */
