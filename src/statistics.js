7f140093a20c74eb0d271a5178b36382e6593fd1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 09:00:26 -0700

    add methods

--- src/statistics.js
+++ src/statistics.js
@@ -1,5 +1,32 @@
 import { logger } from './logger.js';
 import { addOrEditPuzzle, findPuzzle } from './persistence/puzzleRepository.js';
+import { parsePositiveInt } from './validation/validationUtils.js';
+
+/**
+ * Returns the fastest execution time for the puzzle.
+ * Returns null if the puzzle has not been correctly solved.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ */
+export const getFastestExecutionTime = async (year, day, part) => {
+  const puzzle = await findPuzzle(year, day, part);
+  return puzzle?.fastestExecutionTimeNs || null;
+};
+
+/**
+ * Sets the fastest execution time for the puzzle.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ * @param {Number} timeNs
+ */
+export const setFastestExecutionTime = async (year, day, part, timeNs) => {
+  const parsed = parsePositiveInt(timeNs);
+  const puzzle = await findPuzzle(year, day, part);
+  const updated = { ...puzzle, fastestExecutionTimeNs: parsed };
+  return addOrEditPuzzle(updated);
+};
 
 /**
  *
