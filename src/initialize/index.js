ba0cdd396d56b63a8b6dc998b60068122990c5b2
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 14:22:59 -0700

    export helper function

--- src/initialize/index.js
+++ src/initialize/index.js
@@ -7,13 +7,19 @@ import { createSolutionFiles } from './createSolutionFiles.js';
 import { cwdIsEmpty } from './cwdIsEmpty.js';
 import { installPackages } from './installPackages.js';
 
+/**
+ * Creates all of the files necessary for this cli to run.
+ */
+export const createFiles = (...args) => Promise.all([
+  createDataFile(...args),
+  createDotEnv(...args),
+  createGitIgnore(...args),
+  createPackageJson(...args),
+  createReadme(...args),
+  createSolutionFiles(...args),
+]);
+
 export {
-  createDataFile,
-  createDotEnv,
-  createGitIgnore,
-  createPackageJson,
-  createReadme,
-  createSolutionFiles,
   cwdIsEmpty,
   installPackages,
 };
