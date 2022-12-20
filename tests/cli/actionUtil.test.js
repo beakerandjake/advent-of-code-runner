96dc486ca928d199fa90d7c6cf2da672308a7961
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-19 20:49:13 -0700

    test puzzle is unlocked

--- tests/cli/actionUtil.test.js
+++ tests/cli/actionUtil.test.js
@@ -6,19 +6,20 @@ import { mockLogger } from '../mocks.js';
 // setup mocks
 mockLogger();
 jest.unstable_mockModule('src/config.js', () => ({ getConfigValue: jest.fn() }));
+jest.unstable_mockModule('src/answers.js', () => ({ requiredPartsHaveBeenSolved: jest.fn() }));
 jest.unstable_mockModule('src/validation/index.js', () => ({
   yearIsValid: jest.fn(),
   puzzleIsInFuture: jest.fn(),
 }));
 
-// import { requiredPartsHaveBeenSolved } from '../../answers.js';
+const { requiredPartsHaveBeenSolved } = await import('../../src/answers.js');
 const { getConfigValue } = await import('../../src/config.js');
 // import { humanizeDuration } from '../../formatting.js';
 // import { getInput } from '../../inputs/index.js';
 // import { logger } from '../../logger.js';
 // import { executeUserSolution } from '../../solutions/index.js';
 const { puzzleIsInFuture, yearIsValid } = await import('../../src/validation/index.js');
-const { getYear } = await import('../../src/cli/actions/actionUtil.js');
+const { getYear, puzzleIsUnlocked } = await import('../../src/cli/actions/actionUtil.js');
 
 describe('actionUtil', () => {
   describe('getYear()', () => {
@@ -32,6 +33,20 @@ describe('actionUtil', () => {
     test('throws if not valid', () => {
       yearIsValid.mockReturnValue(false);
       expect(() => getYear()).toThrow(RangeError);
+    });
+  });
+
+  describe('puzzleIsUnlocked()', () => {
+    test.each([
+      [true, true, false],
+      [true, false, false],
+      [false, false, false],
+      [false, true, true],
+    ])('puzzle future: %s, required parts: %s returns: %s', async (future, required, expected) => {
+      puzzleIsInFuture.mockReturnValue(future);
+      requiredPartsHaveBeenSolved.mockReturnValue(required);
+      const result = await puzzleIsUnlocked(2022, 1, 1);
+      expect(result).toBe(expected);
     });
   });
 });
