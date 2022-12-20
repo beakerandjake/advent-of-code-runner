6d71752fd395ae2a7afd13aab951db40a0df6bbb
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-19 19:23:52 -0700

    jest absolute imports

--- jest.config.mjs
+++ jest.config.mjs
@@ -72,9 +72,10 @@ export default {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules",
+    "<rootDir>"
+  ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
