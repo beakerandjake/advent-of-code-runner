a2baaa17e009c504c6b4254bef6063fff806380c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-19 18:51:13 -0700

    add command

--- src/cli/commands.js
+++ src/cli/commands.js
@@ -1,7 +1,13 @@
 import { Command } from 'commander';
-import { autoSolve } from './actions/autoSolve.js';
+import { dayArgument, partArgument } from './arguments.js';
+import { solve } from './actions/solve.js';
 
-export const autoSolveCommand = new Command()
-  .name('autosolve')
-  .description('Solve the next unsolved puzzle, benchmarks the execution time, and outputs the result.')
-  .action(autoSolve);
+/**
+ * Command which lets the user solve a specific puzzle
+ */
+export const solveCommand = new Command()
+  .name('solve')
+  .description('Solve the puzzle, benchmark the execution time, and output the result.')
+  .addArgument(dayArgument)
+  .addArgument(partArgument)
+  .action(solve);
