edc91bfe3b9d2c544fd6cd77fa54e35cbe268427
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-20 12:24:31 -0700

    clean up

--- src/actions/solvePuzzle.js
+++ src/actions/solvePuzzle.js
@@ -5,6 +5,34 @@ import { getInputAndExecuteSolution } from './getInputAndExecuteSolution.js';
 import { tryToUpdateFastestExecutionTime } from './tryToUpdateFastestExecutionTime.js';
 
 /**
+ * Checks to see if this puzzle has already been solved by the user
+ * and that the current answer is equal to the correct answer.
+ * @param {Number} year
+ * @param {Number} day
+ * @param {Number} part
+ * @param {String|Number} answer
+ */
+const answerIsCorrectAnswer = async (year, day, part, answer) => {
+  // check to see if the user has correctly submitted an answer to this puzzle.
+  const correctAnswer = await getCorrectAnswer(year, day, part);
+
+  // bail if user has not previously submitted a correct answer.
+  if (!correctAnswer) {
+    return false;
+  }
+
+  if (!answersEqual(answer, correctAnswer)) {
+    // the user might have already submitted the correct answer to this problem
+    // but are re-executing their solution because they made code or performance improvements.
+    // let them know their changes might have broke something.
+    logger.error('You have already correctly answered this puzzle, but answer: "%s" doesn\'t match correct answer: "%s"', answer, correctAnswer);
+    return false;
+  }
+
+  return true;
+};
+
+/**
  * Solve the specified puzzle.
  * @param {Number} day
  * @param {Number} part
@@ -18,25 +46,12 @@ export const solvePuzzle = async (day, part) => {
     return;
   }
 
-  // run the users code.
   const { answer, executionTimeNs } = await getInputAndExecuteSolution(day, part);
 
-  // check to see if the user has correctly submitted an answer to this puzzle.
-  const correctAnswer = await getCorrectAnswer(year, day, part);
-
-  // bail if user has not previously submitted a correct answer.
-  if (!correctAnswer) {
+  // only update statistics if they are re-solved a puzzle they have already completed.
+  if (!await answerIsCorrectAnswer(year, day, part, answer)) {
     return;
   }
 
-  // the user might have already submitted the correct answer to this problem
-  // but are re-executing their solution because they made code or performance improvements.
-  // let them know their changes might have broke something.
-  if (!answersEqual(answer, correctAnswer)) {
-    logger.error('You have already correctly answered this puzzle, but answer: "%s" doesn\'t match correct answer: "%s"', answer, correctAnswer);
-    return;
-  }
-
-  // attempt to update statistics for this puzzle.
   await tryToUpdateFastestExecutionTime(year, day, part, executionTimeNs);
 };
