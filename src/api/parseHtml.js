bdc923ad5522172b5164f3abc0ffe0bfac55c485
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-21 11:12:37 -0700

    add html parsing

--- src/api/parseHtml.js
+++ src/api/parseHtml.js
@@ -1,7 +1,24 @@
-import { convert } from 'html-to-text';
+import { parseDocument } from 'htmlparser2';
+import { findOne, textContent } from 'domutils';
+import { render } from 'dom-serializer';
 
 /**
- * Returns the text content of the html string.n
- * @param {String} responseText
+ * Returns the outerText of the first element with matching tag name in the html.
+ * @param {String} html - The html text to parse.
+ * @param {String} name - The tag name to search for.
+ * @returns {String} The outer html of the element, or null if no matching element found.
  */
-export const getTextContent = (html) => convert(html, { wordwrap: null });
+export const getElementByTagName = (html, name) => {
+  const found = findOne(
+    (element) => element.type === 'tag' && element.name === name,
+    parseDocument(html).children,
+  );
+  return found ? render(found) : null;
+};
+
+/**
+ * Returns the text content of the html string.
+ * @param {String} html - The html to parse
+ * @returns {String} The textContent of the parsed Node
+ */
+export const getTextContent = (html) => textContent(parseDocument(html));
