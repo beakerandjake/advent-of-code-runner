100ab5ccf7adfc2ede0c0216c6e0d11fbd066f30
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-21 09:41:47 -0700

    add todo for response parsing

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,12 @@
 /**
   testing... when mocking async fns, am i returning async fn/promise???
 
+  API Submit Response
+    - Auth Token invalid - parse html or handle status code 302
+    - Day / Year invalid - 404 returned
+    - Wrong Answer - 200 returned, parse html
+    - Too many requests - 200 returned, parse html
+    - Correct Answer - ?
+    - Bad level - 200 returned, parse html
+
  */
