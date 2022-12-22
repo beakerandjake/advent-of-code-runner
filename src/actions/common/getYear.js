83dec65fa1edee109bdbaf18e6b2a4aab1eda6a0
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-22 15:23:50 -0700

    update logging

--- src/actions/common/getYear.js
+++ src/actions/common/getYear.js
@@ -4,15 +4,16 @@ import { yearIsValid } from '../../validation/index.js';
 
 /**
  * Grabs the year value from the config, validates it, then adds it to the args.
- * @throws {RangeError}
+ * @throws {RangeError} The year was not valid
  */
 export const getYear = (args = {}) => {
+  logger.debug('getting year from config');
   const year = getConfigValue('aoc.year');
 
   if (!yearIsValid(year)) {
     throw new RangeError(`The year: ${year} is invalid, check your .env file to ensure you have specified a valid year.`);
   }
 
-  logger.debug('using year value from config: %s', year);
+  logger.debug('got year value of: %s', year);
   return { ...args, year };
 };
