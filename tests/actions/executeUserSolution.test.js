3aeb21e81ee9a1f03187f63ac6b2ec522db3431f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-22 16:53:05 -0700

    add tests

---
+++ tests/actions/executeUserSolution.test.js
@@ -0,0 +1,30 @@
+import {
+  describe, jest, test, afterEach,
+} from '@jest/globals';
+import { mockLogger } from '../mocks.js';
+
+// setup mocks
+mockLogger();
+const mockExecuter = jest.fn();
+jest.unstable_mockModule('src/solutions/index.js', () => ({ executeUserSolution: mockExecuter }));
+
+// import after mocks set up
+const { executeUserSolution } = await import('../../src/actions/common/executeUserSolution.js');
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('actions', () => {
+  describe('common', () => {
+    describe('executeUserSolution()', () => {
+      test('adds results to args', async () => {
+        const args = { day: 1, part: 1, input: 'ASDF' };
+        const executionResult = { answer: '1234', executionTimeNs: 1234 };
+        mockExecuter.mockResolvedValue(executionResult);
+        const result = await executeUserSolution(args);
+        expect(result).toEqual({ ...args, ...executionResult });
+      });
+    });
+  });
+});
