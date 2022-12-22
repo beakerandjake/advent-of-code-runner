c0841267d420bb7ebb98626d0f204ff3409e6324
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-22 14:20:41 -0700

    update tests

--- tests/actions/actionChain.test.js
+++ tests/actions/actionChain.test.js
@@ -43,80 +43,63 @@ describe('actionChain', () => {
     });
   });
 
-  // describe('runAction()', () => {
-  //   test('invokes each link in pre-action chain', async () => {
-  //     const chain = [jest.fn(), jest.fn(), jest.fn()];
-  //     await executeChain({}, () => {}, chain);
-  //     chain.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
-  //   });
+  describe('runAction()', () => {
+    test('does not throw if chain is empty', async () => {
+      await expect(async () => executeChain([], {})).resolves;
+    });
 
-  //   test('invokes action if no chain', async () => {
-  //     const action = jest.fn();
-  //     await executeChain({}, action, []);
-  //     expect(action).toHaveBeenCalledTimes(1);
-  //   });
+    test('invokes each link in pre-action chain', async () => {
+      const chain = [jest.fn(), jest.fn(), jest.fn()];
+      await executeChain(chain, {});
+      chain.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
+    });
 
-  //   test('invokes action if has chain (none throw)', async () => {
-  //     const action = jest.fn();
-  //     await executeChain({}, action, [jest.fn(), jest.fn(), jest.fn(), jest.fn()]);
-  //     expect(action).toHaveBeenCalledTimes(1);
-  //   });
+    test('invokes each link', async () => {
+      const chain = [jest.fn(), jest.fn(), jest.fn(), jest.fn()];
+      await executeChain(chain);
+      chain.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
+    });
 
-  //   test('throws if any link throws', async () => {
-  //     const chain = [jest.fn(), jest.fn(() => Promise.reject(new RangeError())), jest.fn()];
-  //     await expect(async () => executeChain({}, () => {}, chain)).rejects.toThrow(RangeError);
-  //   });
+    test('link can halt chain', async () => {
+      const before = [jest.fn(), jest.fn(), jest.fn()];
+      const halter = jest.fn(() => false);
+      const after = [jest.fn(), jest.fn(), jest.fn(), jest.fn()];
+      await executeChain([...before, halter, ...after]);
+      before.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
+      expect(halter).toHaveBeenCalledTimes(1);
+      after.forEach((x) => expect(x).not.toHaveBeenCalled());
+    });
 
-  //   test('halts chain if any link throws', async () => {
-  //     const before = [jest.fn(), jest.fn()];
-  //     const after = [jest.fn(), jest.fn()];
-  //     const bomb = jest.fn(() => Promise.reject(new Error()));
-  //     const chain = [...before, bomb, ...after];
-  //     await expect(async () => executeChain({}, () => {}, chain)).rejects.toThrow();
-  //     before.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
-  //     expect(bomb).toHaveBeenCalled();
-  //     expect(bomb).rejects.toThrow();
-  //     after.forEach((x) => expect(x).not.toHaveBeenCalled());
-  //   });
+    test('throws if any link throws', async () => {
+      const chain = [jest.fn(), jest.fn(() => Promise.reject(new RangeError())), jest.fn()];
+      await expect(async () => executeChain(chain)).rejects.toThrow(RangeError);
+    });
 
-  //   test('does not invoke action if chain throws', async () => {
-  //     const chain = [() => {}, jest.fn(() => Promise.reject(new Error()))];
-  //     const action = jest.fn();
-  //     await expect(async () => executeChain({}, action, chain)).rejects.toThrow();
-  //     expect(action).not.toHaveBeenCalled();
-  //   });
+    test('halts chain if any link throws', async () => {
+      const before = [jest.fn(), jest.fn()];
+      const bomb = jest.fn(() => Promise.reject(new Error()));
+      const after = [jest.fn(), jest.fn()];
+      await expect(async () => executeChain([...before, bomb, ...after])).rejects.toThrow();
+      before.forEach((x) => expect(x).toHaveBeenCalledTimes(1));
+      expect(bomb).toHaveBeenCalled();
+      after.forEach((x) => expect(x).not.toHaveBeenCalled());
+    });
 
-  //   test('passes args through chain', async () => {
-  //     const args = { cats: true, dogs: false };
-  //     const chain = [jest.fn(), jest.fn(), jest.fn()];
-  //     await executeChain(args, () => {}, chain);
-  //     chain.forEach((x) => expect(x).toHaveBeenCalledWith(args));
-  //   });
+    test('passes args through chain', async () => {
+      const args = { cats: true, dogs: false };
+      const chain = [jest.fn(), jest.fn(), jest.fn()];
+      await executeChain(chain, args);
+      chain.forEach((x) => expect(x).toHaveBeenCalledWith(args));
+    });
 
-  //   test('link in chain can mutate args for future links', async () => {
-  //     const origArgs = { cats: true, dogs: false };
-  //     const mutatedArgs = { ...origArgs, apples: true };
-  //     const before = [jest.fn(), jest.fn(), jest.fn(() => mutatedArgs)];
-  //     const after = [jest.fn(), jest.fn()];
-  //     await executeChain(origArgs, () => {}, [...before, ...after]);
-  //     before.forEach((x) => expect(x).toHaveBeenCalledWith(origArgs));
-  //     after.forEach((x) => expect(x).toHaveBeenCalledWith(mutatedArgs));
-  //   });
-
-  //   test('passes args to action', async () => {
-  //     const args = { cats: true, dogs: false };
-  //     const actionFn = jest.fn();
-  //     await executeChain(args, actionFn, [jest.fn(), jest.fn(), jest.fn()]);
-  //     expect(actionFn).toHaveBeenCalledWith(args);
-  //   });
-
-  //   test('passes mutated args to action', async () => {
-  //     const origArgs = { cats: true, dogs: false };
-  //     const mutatedArgs = { ...origArgs, apples: true };
-  //     const chain = [jest.fn(), jest.fn(), jest.fn(() => mutatedArgs)];
-  //     const actionFn = jest.fn();
-  //     await executeChain(origArgs, actionFn, chain);
-  //     expect(actionFn).toHaveBeenCalledWith(mutatedArgs);
-  //   });
-  // });
+    test('link in chain can mutate args for future links', async () => {
+      const origArgs = { cats: true, dogs: false };
+      const mutatedArgs = { ...origArgs, apples: true };
+      const before = [jest.fn(), jest.fn(), jest.fn(() => mutatedArgs)];
+      const after = [jest.fn(), jest.fn()];
+      await executeChain([...before, ...after], origArgs);
+      before.forEach((x) => expect(x).toHaveBeenCalledWith(origArgs));
+      after.forEach((x) => expect(x).toHaveBeenCalledWith(mutatedArgs));
+    });
+  });
 });
