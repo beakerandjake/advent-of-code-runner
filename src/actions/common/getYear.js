b28db5b7218f762a612b694382089434842f3c78
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-22 14:35:11 -0700

    add first common action

---
+++ src/actions/common/getYear.js
@@ -0,0 +1,19 @@
+import { getConfigValue } from '../../config.js';
+import { logger } from '../../logger.js';
+import { yearIsValid } from '../../validation/index.js';
+
+/**
+ * Grabs the year value from the config, validates it, then adds it to the args.
+ * @throws {RangeError}
+ */
+export const getYear = (args) => {
+  const year = getConfigValue('aoc.year');
+
+  if (!yearIsValid(year)) {
+    throw new RangeError(`The year: ${year} is invalid, check your .env file to ensure you have specified a valid year.`);
+  }
+
+  logger.debug('setting year to: %s', year);
+
+  return { ...args, year };
+};
---
+++ src/actions/common/index.js
@@ -0,0 +1,9 @@
+import { getYear } from './getYear.js';
+
+/**
+ * barrel for all common actions which are used or could be used by multiple action chains.
+ */
+
+export {
+  getYear,
+};
