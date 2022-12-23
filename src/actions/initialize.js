7a73a4c3938d54e548eaf69214658c378e8f8676
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-23 13:21:28 -0700

    clean up initialize

--- src/actions/initialize.js
+++ src/actions/initialize.js
@@ -1,70 +1,105 @@
-// import ora from 'ora';
-// import { confirmWithUser, getAnswersFromUser } from './interactive.js';
-// import { initializeQuestions, confirmOverwriteCwdQuestion } from './initializeQuestions.js';
-// import { createFiles, cwdIsEmpty, installPackages } from '../initialize/index.js';
-// import { festiveStyle, festiveEmoji } from '../festive.js';
-
-// /**
-//  * Scaffolds the cwd with all files required to use this cli.
-//  */
-// export const initialize = async () => {
-//   // if there are files in the cwd, get confirmation with the user that they want to proceed.
-//   if (!await cwdIsEmpty() && !await confirmWithUser(confirmOverwriteCwdQuestion)) {
-//     return;
-//   }
-
-//   const answers = await getAnswersFromUser(initializeQuestions);
-//   const spinner = ora({ text: festiveStyle('Creating files'), spinner: 'christmas' }).start();
-
-//   try {
-//     await createFiles(answers);
-//     spinner.text = festiveStyle('Installing packages');
-//     await installPackages();
-//     spinner.stopAndPersist({
-//       text: festiveStyle('Successfully initialized your repository, have fun!'),
-//       symbol: festiveEmoji(),
-//     });
-//   } catch (error) {
-//     spinner.fail();
-//     throw error;
-//   }
-// };
-
-import { getYear } from './links/index.js';
+import ora from 'ora';
+import { getConfigValue } from '../config.js';
+import { festiveEmoji, festiveStyle } from '../festive.js';
+import {
+  createDataFile,
+  createDotEnv,
+  createGitIgnore,
+  createPackageJson,
+  createReadme,
+  createSolutionFiles,
+  installPackages,
+} from '../initialize/index.js';
 import { createChain } from './actionChain.js';
+import { getAnswersFromUser } from './links/getAnswersFromUser.js';
+import {
+  assertInitialized,
+  assertUserConfirmation,
+  not,
+  or,
+} from './links/index.js';
 
-const spinnerDecorator = () => async (...args) => {
-  // TODO create spinner, pass args to chain { updateProgressText: function }
-  // on chain finish, stop and persist spinner
-  // on chain fail, fail spinner and re-throw
-};
-
-const coolFn = (args) => {
-  console.log('coolFn args:', args);
-};
-
-const coolFn2 = (...args) => {
-  console.log('coolFn2 args:', args);
-};
-
-const coolFn3 = (...args) => {
-  console.log('coolFn3 args:', ...args);
+/**
+ * inquirer.js question which makes the user confirm the initialize operation.
+ */
+export const confirmInitializeQuestion = {
+  type: 'confirm',
+  name: 'confirmed',
+  message: festiveStyle(
+    'This directory is not empty! This operation will overwrite files, do you want to continue?',
+  ),
+  default: false,
+  prefix: festiveEmoji(),
 };
 
 /**
- *
+ * Array of inquirer questions which will be asked in order.
+ * The answers will provide us all of the information we need to initialize.
  */
-const actionChain = createChain([
-  getYear,
-  // () => {
-  //   const args = { cat: true, dog: false, cow: 'moo' };
-  //   coolFn(args);
-  //   coolFn2(args);
-  //   coolFn3(args);
-  // },
-]);
+export const initializeQuestions = [
+  {
+    // in future if list of years becomes too large the change to raw input.
+    type: 'list',
+    name: 'year',
+    message: festiveStyle('What year of advent of code are you doing?'),
+    prefix: festiveEmoji(),
+    choices: getConfigValue('aoc.validation.years').reverse(),
+    loop: false,
+  },
+  {
+    // in future if list of years becomes too large the change to raw input.
+    type: 'password',
+    name: 'authToken',
+    message: festiveStyle('Enter your advent of code authentication token'),
+    prefix: festiveEmoji(),
+    choices: getConfigValue('aoc.validation.years'),
+    loop: false,
+    validate: (input) => (input ? true : 'Token cannot be empty!'),
+    filter: (input) => input.trim(),
+  },
+];
+
+/**
+ * A Link which creates all required files in the cwd.
+ */
+const createFiles = async ({ answers }) => {
+  await Promise.all([
+    createDataFile(answers),
+    createDotEnv(answers),
+    createGitIgnore(answers),
+    createPackageJson(answers),
+    createReadme(answers),
+    createSolutionFiles(answers),
+  ]);
+};
 
 /**
  * Performs first time setup, scaffolds the cwd with all of the files required to run this cli.
  */
-export const initialize = () => actionChain({});
+export const initialize = async () => {
+  // could be some long running operations here, we will use a spinner
+  // to let the user know it's not frozen.
+  const spinner = ora({ spinner: 'christmas' });
+
+  const actionChain = createChain([
+    // halt the chain the cwd is not empty and the user does not confirm
+    or(not(assertInitialized), assertUserConfirmation(confirmInitializeQuestion)),
+    // grab year / auth token from user.
+    getAnswersFromUser(initializeQuestions),
+    // create all required files in the cwd.
+    () => spinner.start(festiveStyle('Creating files')),
+    createFiles,
+    // install npm packages for the user.
+    () => { spinner.text = festiveStyle('Installing packages'); },
+    installPackages,
+  ]);
+
+  try {
+    const completed = await actionChain();
+    // make sure we stop the spinner when the chain finishes.
+    completed && spinner.succeed(festiveStyle('Successfully initialized your repository, have fun!'));
+  } catch (error) {
+    spinner.fail();
+    throw error;
+  }
+};
