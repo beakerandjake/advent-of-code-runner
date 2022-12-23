28247e8e1f00c703963bae5d679f1ca36d08b879
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-23 12:01:02 -0700

    add logging

--- src/actions/links/assertInitialized.js
+++ src/actions/links/assertInitialized.js
@@ -5,9 +5,12 @@ import { dataStoreFileExists } from '../../persistence/jsonFileStore.js';
  * Asserts that the user has ran an init command in the cwd.
  */
 export const assertInitialized = async () => {
-  logger.debug('checking if initialized');
-  // it's probably enough to check if the data store file exists
-  // not worth it to check packages installed or src files exist etc.
-  // we can be pretty sure they have ran init if the data store file is there.
-  return dataStoreFileExists();
+  /**
+   * it's probably enough to check if the data store file exists
+   * not worth it to check packages installed or src files exist etc.
+   * we can be pretty sure they have ran init if the data store file is there.
+   */
+  const initialized = await dataStoreFileExists();
+  logger.debug('cwd is initialized: %s', initialized);
+  return initialized;
 };
