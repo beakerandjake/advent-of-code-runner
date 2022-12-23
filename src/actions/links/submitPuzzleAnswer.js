041e2731d52c6100a84c36c61698aa8f10cd3bc4
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-23 16:37:10 -0700

    fix param name

--- src/actions/links/submitPuzzleAnswer.js
+++ src/actions/links/submitPuzzleAnswer.js
@@ -5,10 +5,10 @@ import { logger } from '../../logger.js';
  * Submit the answer to advent of code and returns the submission result.
  */
 export const submitPuzzleAnswer = async ({
-  year, day, part, answer, authenticationToken,
+  year, day, part, answer, authToken,
 } = {}) => {
   logger.festive('Submitting your answer to advent of code');
-  const { success, message } = await submitSolution(year, day, part, answer, authenticationToken);
+  const { success, message } = await submitSolution(year, day, part, answer, authToken);
   logger[success ? 'festive' : 'error'](message);
   return { submissionResult: { success, message } };
 };
