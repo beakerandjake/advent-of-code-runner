4883074b7ff0f6ce088684a2cf32fee3adcef640
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-23 12:14:11 -0700

    clean up

--- src/actions/links/assertUserConfirmation.js
+++ src/actions/links/assertUserConfirmation.js
@@ -8,9 +8,11 @@ import inquirer from 'inquirer';
 export const assertUserConfirmation = (question) => {
   // create a variable for this fn instead of just returning the fn
   // this gives the fn a .name property and makes debugging easier.
-  const confirmWithUser = async () => {
-    const { confirmed } = await inquirer.prompt({ ...question, type: 'confirm' });
-    return confirmed;
+  const _ = {
+    assertUserConfirmation: async () => {
+      const { confirmed } = await inquirer.prompt({ ...question, type: 'confirm' });
+      return confirmed;
+    },
   };
-  return confirmWithUser;
+  return _.assertUserConfirmation;
 };
