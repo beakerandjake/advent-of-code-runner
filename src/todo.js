1342775246f38cdd387245fa60b734f7ffe783d8
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-22 21:44:53 -0700

    update todo

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,5 @@
 /**
   - testing... when mocking async fns, am i returning async fn/promise???
+  - initialize command update to use action link
   - After solving correctly, check solving again..
  */
