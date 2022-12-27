48ce2a73eedab18ac4e0fff141e155281232bd6f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-26 17:57:27 -0700

    add puzzle link

--- src/initialize/createSolutionFiles.js
+++ src/initialize/createSolutionFiles.js
@@ -1,25 +1,41 @@
 import { join } from 'node:path';
 import { getConfigValue } from '../config.js';
-import { copyFile, ensureDirectoriesExist } from '../persistence/io.js';
+import {
+  ensureDirectoriesExist, loadFileContents, saveFile,
+} from '../persistence/io.js';
 import { logger } from '../logger.js';
+import { replaceTokens } from './replaceTokens.js';
+
+/**
+ * Maps tokens strings in the template project json file to fields of the args.
+ */
+const tokens = [
+  { match: '{{day}}', key: 'day' },
+  { match: '{{year}}', key: 'year' },
+];
 
 /**
  * Creates the solution files in the cwd.
  */
-export const createSolutionFiles = async () => {
+export const createSolutionFiles = async ({ year }) => {
   logger.debug('creating Solution files');
 
+  // create directory if doesn't exist.
   const solutionsDir = getConfigValue('paths.solutionsDir');
-
   await ensureDirectoriesExist(solutionsDir);
 
-  const filePaths = getConfigValue('aoc.validation.days').map(
-    (day) => join(solutionsDir, `day_${day}.js`),
+  // load the contents of the template solution
+  const templateSolutionFile = await loadFileContents(
+    getConfigValue('paths.templates.solution'),
   );
 
-  const templateSolutionFile = getConfigValue('paths.templates.solution');
-
-  await Promise.all(
-    filePaths.map((dest) => copyFile(templateSolutionFile, dest)),
+  // create each template solution file.
+  const createFilePromises = getConfigValue('aoc.validation.days').map(
+    (day) => saveFile(
+      join(solutionsDir, `day_${day}.js`),
+      replaceTokens(tokens, { year, day }, templateSolutionFile),
+    ),
   );
+
+  await Promise.all(createFilePromises);
 };
