ec3a07d9fc576b3f648a69156d12fd50df10935c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-28 11:53:35 -0700

    add todo

--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,7 @@
 /**
- * - testing... when mocking async fns, am i returning async fn/promise???
- * - After solving correctly, check solving again..
- *
- * todo:
- * 83, 81, 80, 77, 62, 49
- *
- *
+ * Use fs-extra, remove io.js
+ * add userDataFile.js
+ *  - cache over json file
+ * rateLimitRepository, writes / reads json file directly
+ *  - not as important to have caching, single file only touched in one place.
  */
