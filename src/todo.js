9195ef4d5c171949784e1aa962c258f6470b3feb
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-29 11:45:00 -0700

    remove todo

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,3 @@
 /**
  * Use fs-extra, remove io.js
- * add userDataFile.js
- *  - cache over json file
- * rateLimitRepository, writes / reads json file directly
- *  - not as important to have caching, single file only touched in one place.
  */
