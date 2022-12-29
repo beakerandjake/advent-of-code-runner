e41d518ef6e944e431d2b7bb2d6c3277b9c50ff1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-12-29 11:44:04 -0700

    passing tests

--- tests/persistence/rateLimitRepository.test.js
+++ tests/persistence/rateLimitRepository.test.js
@@ -60,59 +60,42 @@ describe('rateLimitRepository', () => {
     });
   });
 
-  // describe('setRateLimit()', () => {
-  //   test.each([
-  //     null,
-  //     undefined,
-  //     Promise.resolve(new Date()),
-  //     true,
-  //     1234,
-  //     '12/01/2022',
-  //     {},
-  //     new Date(Infinity),
-  //   ])('throws on invalid date value: "%s"', async (value) => {
-  //     expect(async () => setRateLimit('asdf', value)).rejects.toThrow(TypeError);
-  //   });
+  describe('setRateLimit()', () => {
+    test.each([
+      null,
+      undefined,
+      Promise.resolve(new Date()),
+      true,
+      1234,
+      '12/01/2022',
+      {},
+      new Date(Infinity),
+    ])('throws on invalid date value: "%s"', async (value) => {
+      expect(async () => setRateLimit('asdf', value)).rejects.toThrow(TypeError);
+    });
 
-  //   // test('throws on null/undefined expiration', () => {
-  //   //   expect(async () => setRateLimit('asdf', null)).rejects.toThrow(TypeError);
-  //   //   expect(async () => setRateLimit('asdf', undefined)).rejects.toThrow(TypeError);
-  //   // });
+    test('adds if didn\'t exist', async () => {
+      const key = 'cool';
+      const value = new Date(2022, 11, 4);
+      const orig = { notCool: new Date().toISOString() };
+      readJson.mockResolvedValueOnce(orig);
+      await setRateLimit(key, value);
+      expect(outputJson).toHaveBeenCalledWith(
+        expect.any(String),
+        { ...orig, [key]: value.toISOString() },
+      );
+    });
 
-  //   // test('throws on expiration is not a date ', () => {
-  //   //   expect(async () => setRateLimit('asdf', Promise.resolve(new Date()))).rejects.toThrow(TypeError);
-  //   //   expect(async () => setRateLimit('asdf', true)).rejects.toThrow(TypeError);
-  //   //   expect(async () => setRateLimit('asdf', 1234234)).rejects.toThrow(TypeError);
-  //   //   expect(async () => setRateLimit('asdf', '12/01/2022')).rejects.toThrow(TypeError);
-  //   //   expect(async () => setRateLimit('asdf', {})).rejects.toThrow(TypeError);
-  //   // });
-
-  //   // test('throws on expiration is invalid date', () => {
-  //   //   expect(async () => setRateLimit('asdf', new Date(Infinity))).rejects.toThrow(TypeError);
-  //   // });
-
-  //   test('adds if didn\'t exist', async () => {
-  //     const key = 'cool';
-  //     const value = new Date(2022, 11, 4);
-  //     const orig = { notCool: new Date().toISOString() };
-  //     getValue.mockResolvedValueOnce({ notCool: new Date().toISOString() });
-  //     await setRateLimit(key, value);
-  //     expect(setValue).toHaveBeenCalledWith(
-  //       expect.any(String),
-  //       { ...orig, [key]: value.toISOString() },
-  //     );
-  //   });
-
-  //   test('updates if already exists', async () => {
-  //     const key = 'cool';
-  //     const value = new Date(2022, 11, 4);
-  //     const orig = { notCool: new Date().toISOString(), [key]: value.toISOString() };
-  //     getValue.mockResolvedValueOnce({ notCool: new Date().toISOString() });
-  //     await setRateLimit(key, value);
-  //     expect(setValue).toHaveBeenCalledWith(
-  //       expect.any(String),
-  //       { ...orig, [key]: value.toISOString() },
-  //     );
-  //   });
-  // });
+    test('updates if already exists', async () => {
+      const key = 'cool';
+      const value = new Date(2022, 11, 4);
+      const orig = { notCool: new Date().toISOString(), [key]: value.toISOString() };
+      readJson.mockResolvedValueOnce(orig);
+      await setRateLimit(key, value);
+      expect(outputJson).toHaveBeenCalledWith(
+        expect.any(String),
+        { ...orig, [key]: value.toISOString() },
+      );
+    });
+  });
 });
