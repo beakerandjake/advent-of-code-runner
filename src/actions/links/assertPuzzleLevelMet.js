e92cb82669fc323b4ab5d723eac16be46f3dd4c4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-04 12:46:12 -0700

    update logging

--- src/actions/links/assertPuzzleLevelMet.js
+++ src/actions/links/assertPuzzleLevelMet.js
@@ -5,10 +5,12 @@ import { logger } from '../../logger.js';
  * Asserts that the user has solved the requisite levels leading up to this puzzles level.
  */
 export const assertPuzzleLevelMet = async ({ year, day, part } = {}) => {
+  logger.verbose('checking if puzzle level is unlocked');
+
   if (!await requiredPartsHaveBeenSolved(year, day, part)) {
     logger.error(`You cannot attempt this puzzle (day ${day}, level ${part}) because you have not completed the previous levels of this puzzle.`);
     return false;
   }
-  logger.verbose('required levels leading up to this one have been solved.');
+  logger.verbose('puzzle level is unlocked, all required levels leading up to this one have been solved.');
   return true;
 };
