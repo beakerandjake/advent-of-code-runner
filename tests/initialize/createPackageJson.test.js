34c84307197cbfb816261f5fdfbe28f1661768c3
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-04 11:08:53 -0700

    update tests

--- tests/initialize/createPackageJson.test.js
+++ tests/initialize/createPackageJson.test.js
@@ -7,10 +7,19 @@ import { mockConfig, mockLogger } from '../mocks.js';
 mockLogger();
 const { getConfigValue } = mockConfig();
 jest.unstable_mockModule('node:child_process', () => ({ exec: jest.fn() }));
+jest.unstable_mockModule('node:path', () => ({ join: jest.fn() }));
+jest.unstable_mockModule('fs-extra/esm', () => ({ readJson: jest.fn(), writeJson: jest.fn() }));
 
 // import after mocks set up
 const { exec } = await import('node:child_process');
+const { readJson, writeJson } = await import('fs-extra/esm');
 const { createPackageJson } = await import('../../src/initialize/createPackageJson.js');
+
+// import { readJson, writeJson } from 'fs-extra/esm';
+// import { join } from 'node:path';
+// import { exec } from 'node:child_process';
+// import { getConfigValue } from '../config.js';
+// import { logger } from '../logger.js';
 
 describe('initialize', () => {
   describe('createPackageJson()', () => {
@@ -26,14 +35,17 @@ describe('initialize', () => {
 
     test('resolves promise when no exec error', async () => {
       exec.mockImplementation((command, options, callback) => callback());
+      readJson.mockResolvedValue({});
       const result = createPackageJson({ year: 2022 });
       await expect(result).resolves.not.toThrow();
+      expect(writeJson).toHaveBeenCalled();
     });
 
     test('throws if exec error', async () => {
       exec.mockImplementation((command, options, callback) => callback(new Error()));
       const result = createPackageJson({ year: 2022 });
       await expect(result).rejects.toThrow();
+      expect(writeJson).not.toHaveBeenCalled();
     });
   });
 });
