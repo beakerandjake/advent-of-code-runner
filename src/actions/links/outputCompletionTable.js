9256abad4fcafabf25ca314d504dbd7e36ee8002
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-06 10:08:08 -0700

    wip output table cleanup

--- src/actions/links/outputCompletionTable.js
+++ src/actions/links/outputCompletionTable.js
@@ -1,77 +1,6 @@
-import chalk from 'chalk';
-import { table } from 'table';
-import { humanizeDuration } from '../../formatting.js';
+import { logger } from '../../logger.js';
 import { getPuzzleCompletionData, summarizeCompletionData } from '../../statistics.js';
-
-/**
- * Returns text for the "Puzzle" column.
- */
-const puzzleNameText = (day, part) => `${day}.${part}`;
-
-/**
- * Returns text for the "Solved" column.
- */
-const solvedText = (solved) => `${solved ? chalk.green('✓') : ''}`;
-
-/**
- * Returns text for the "# of Attempts" column.
- */
-const numberOfAttemptsText = (numberOfAttempts, maxNumberOfAttempts) => {
-  if (numberOfAttempts == null) {
-    return '';
-  }
-
-  if (maxNumberOfAttempts > 1 && numberOfAttempts === maxNumberOfAttempts) {
-    return chalk.yellow(`${numberOfAttempts} (worst)`);
-  }
-
-  if (numberOfAttempts === 1) {
-    return chalk.green(numberOfAttempts);
-  }
-
-  return numberOfAttempts;
-};
-
-/**
- * Returns text for the "Execution Time" column.
- */
-const executionTimeText = (executionTimeNs, slowest, fastest) => {
-  if (executionTimeNs == null) {
-    return '';
-  }
-  const text = humanizeDuration(executionTimeNs);
-
-  if (fastest > 0 && executionTimeNs === fastest) {
-    return chalk.green(`${text} (best)`);
-  }
-
-  if (slowest > 0 && executionTimeNs === slowest) {
-    return chalk.yellow(`${text} (worst)`);
-  }
-
-  return text;
-};
-
-/**
- * Converts a row of puzzle completion data to the format expected by our table
- */
-const toTableRow = (completionRow, completionSummary) => ([
-  puzzleNameText(completionRow.day, completionRow.part),
-  solvedText(completionRow.solved),
-  numberOfAttemptsText(completionRow.numberOfAttempts, completionSummary.maxAttempts),
-  executionTimeText(
-    completionRow.executionTimeNs,
-    completionSummary.minExecutionTime,
-    completionSummary.maxExecutionTime,
-  ),
-]);
-
-/**
- * Generates text for the solved row.
- */
-const solvedRowText = ({ numberSolved, totalPuzzles, percentSolved }) => (
-  `Solved ${numberSolved}/${totalPuzzles} (${(percentSolved * 100).toFixed()}%)`
-);
+import { generateTable } from '../../tables/cliCompletionTable.js';
 
 /**
  * Outputs a table to the cli which shows the users progress for the year.
@@ -80,49 +9,15 @@ export const outputCompletionTable = async ({ year } = {}) => {
   if (year == null) {
     throw new Error('null or undefined year');
   }
-
   const completionData = await getPuzzleCompletionData(year);
+
+  if (completionData.length === 0) {
+    logger.festive('You have not submitted any puzzles yet, please run this command after submitting a puzzle');
+    return false;
+  }
+
   const summaryData = summarizeCompletionData(completionData);
-
-  const headerRows = [
-    [chalk.green(`Advent of Code ${year}`), '', '', ''],
-    ['Puzzle', 'Solved', 'Attempts', 'Execution Time'].map((x) => chalk.blue(x)),
-  ];
-
-  const puzzleRows = completionData.map((x) => toTableRow(x, summaryData));
-
-  const summaryRows = [
-    ['Average',
-      '',
-      numberOfAttemptsText(summaryData.averageNumberOfAttempts.toFixed(2)),
-      executionTimeText(summaryData.averageExecutionTimeNs),
-    ],
-    [solvedRowText(summaryData), '', '', ''],
-  ];
-
-  const tableConfig = {
-    columnDefault: { alignment: 'left' },
-    columns: [
-      { alignment: 'right' },
-      { alignment: 'center' },
-    ],
-    spanningCells: [
-      // Header Row
-      {
-        col: 0, row: 0, colSpan: 4, alignment: 'center',
-      },
-      // Average Row
-      { col: 0, row: headerRows.length + puzzleRows.length, colSpan: 2 },
-      // Solved Row
-      {
-        col: 0, row: 1 + headerRows.length + puzzleRows.length, colSpan: 4, alignment: 'center',
-      },
-    ],
-    // don't draw lines between puzzle rows (makes table more compact)
-    drawHorizontalLine: (lineIndex) => (
-      lineIndex <= headerRows.length || lineIndex >= headerRows.length + puzzleRows.length
-    ),
-  };
-
-  console.log(table([...headerRows, ...puzzleRows, ...summaryRows], tableConfig));
+  const table = generateTable(year, completionData, summaryData);
+  console.log(table);
+  return true;
 };
--- tests/actions/outputCompletionTable.test.js
+++ tests/actions/outputCompletionTable.test.js
@@ -5,10 +5,12 @@ import { mockLogger } from '../mocks.js';
 
 // setup mocks
 mockLogger();
-// jest.unstable_mockModule('src/persistence/userDataFile.js', () => ({ getValue: jest.fn() }));
-// jest.unstable_mockModule('src/validation/index.js', () => ({ yearIsValid: jest.fn() }));
+jest.unstable_mockModule('src/statistics.js', () => ({ getPuzzleCompletionData: jest.fn(), summarizeCompletionData: jest.fn() }));
+jest.unstable_mockModule('src/tables/cliCompletionTable.js', () => ({ generateTable: jest.fn() }));
 
 // import after mocks set up
+const { getPuzzleCompletionData, summarizeCompletionData } = await import('../../src/statistics.js');
+const { generateTable } = await import('../../src/tables/cliCompletionTable.js');
 const { outputCompletionTable } = await import('../../src/actions/links/outputCompletionTable.js');
 
 describe('actions', () => {
@@ -22,6 +24,18 @@ describe('actions', () => {
         null, undefined,
       ])('throws if year is: "%s"', async (year) => {
         await expect(async () => outputCompletionTable({ year })).rejects.toThrow();
+      });
+
+      test('halts chain if no completion data', async () => {
+        getPuzzleCompletionData.mockResolvedValue([]);
+        const result = await outputCompletionTable({ year: 2022 });
+        expect(result).toBe(false);
+      });
+
+      test('does not generate table if no completion data', async () => {
+        getPuzzleCompletionData.mockResolvedValue([]);
+        await outputCompletionTable({ year: 2022 });
+        expect(generateTable).not.toHaveBeenCalled();
       });
     });
   });
