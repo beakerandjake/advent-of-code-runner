ae0134dc06ee33d0898fabb0ea38154ab28d7893
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-11 14:02:49 -0700

    add auth command

--- src/cli/auth.js
+++ src/cli/auth.js
@@ -1,7 +1,9 @@
 import { Command } from 'commander';
+import { assertInitialized } from '../actions/assertInitialized.js';
+import { assertUserConfirmation } from '../actions/assertUserConfirmation.js';
 import { getAnswersFromUser } from '../actions/getAnswersFromUser.js';
 import { festiveEmoji, festiveStyle, printFestiveTitle } from '../festive.js';
-import { cwdIsEmpty, createDotEnv } from '../initialize/index.js';
+import { createDotEnv, dotEnvFileExists } from '../initialize/index.js';
 import { logger } from '../logger.js';
 
 /**
@@ -16,14 +18,36 @@ export const authTokenQuestion = {
   filter: (input) => input.trim(),
 };
 
+/**
+ * inquirer.js question which makes the user confirm the overwriting the .env file
+ */
+const confirmOverwriteQuestion = {
+  type: 'confirm',
+  name: 'confirmed',
+  message: festiveStyle(
+    'It appears a .env file is already present, do you want to overwrite this file?',
+  ),
+  default: false,
+  prefix: festiveEmoji(),
+};
+
+/**
+ * Updates or creates the .env file and writes the users auth token to it.
+ */
 const auth = async () => {
-  if (!await cwdIsEmpty()) {
-    logger.error('This directory does not appear to be initialized, please run the "init" command instead');
+  // don't let the user run this command if they haven't ran the "init" command.
+  if (!await assertInitialized()) {
+    return;
+  }
+
+  // if there is already a .env file then ask users confirmation.
+  if (await dotEnvFileExists() && !await assertUserConfirmation(confirmOverwriteQuestion)()) {
     return;
   }
 
   const { answers } = await getAnswersFromUser([authTokenQuestion])();
   await createDotEnv(answers);
+  logger.festive('Added auth token to the .env file, do not commit this file to source control');
 };
 
 /**
