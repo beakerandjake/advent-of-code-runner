a80ac455244d9807b5821abb200d4bdb97fe6fc6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-12 13:38:15 -0700

    rename level to part

--- tests/actions/submitPuzzleAnswer.test.js
+++ tests/actions/submitPuzzleAnswer.test.js
@@ -11,46 +11,42 @@ jest.unstable_mockModule('src/api/index.js', () => ({ submitSolution: jest.fn() 
 const { submitSolution } = await import('../../src/api/index.js');
 const { submitPuzzleAnswer } = await import('../../src/actions/submitPuzzleAnswer.js');
 
-afterEach(() => {
-  jest.resetAllMocks();
-});
+describe('submitPuzzleAnswer()', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
 
-describe('actions', () => {
-  describe('links', () => {
-    describe('submitPuzzleAnswer()', () => {
-      test.each([
-        null, undefined,
-      ])('throws if auth token is: %s', async (authToken) => {
-        await expect(async () => submitPuzzleAnswer({
-          year: 2022, day: 1, part: 1, answer: 'asdf', authToken,
-        })).rejects.toThrow();
-      });
+  test.each([
+    null, undefined,
+  ])('throws if auth token is: %s', async (authToken) => {
+    await expect(async () => submitPuzzleAnswer({
+      year: 2022, day: 1, level: 1, answer: 'asdf', authToken,
+    })).rejects.toThrow();
+  });
 
-      test.each([
-        null, undefined,
-      ])('throws if answer is: %s', async (answer) => {
-        await expect(async () => submitPuzzleAnswer({
-          year: 2022, day: 1, part: 1, answer, authToken: 'SADF',
-        })).rejects.toThrow();
-      });
+  test.each([
+    null, undefined,
+  ])('throws if answer is: %s', async (answer) => {
+    await expect(async () => submitPuzzleAnswer({
+      year: 2022, day: 1, level: 1, answer, authToken: 'SADF',
+    })).rejects.toThrow();
+  });
 
-      test('returns on answer correct', async () => {
-        const apiResponse = { correct: true, message: 'great job!' };
-        submitSolution.mockReturnValue(apiResponse);
-        const result = await submitPuzzleAnswer({
-          year: 2022, day: 1, part: 1, answer: 'ASDF', authToken: 'asdf',
-        });
-        expect(result).toEqual({ submissionResult: apiResponse });
-      });
-
-      test('returns on answer incorrect', async () => {
-        const apiResponse = { correct: false, message: 'that is wrong!' };
-        submitSolution.mockReturnValue(apiResponse);
-        const result = await submitPuzzleAnswer({
-          year: 2022, day: 1, part: 1, answer: 'ASDF', authToken: 'asdf',
-        });
-        expect(result).toEqual({ submissionResult: apiResponse });
-      });
+  test('returns on answer correct', async () => {
+    const apiResponse = { correct: true, message: 'great job!' };
+    submitSolution.mockReturnValue(apiResponse);
+    const result = await submitPuzzleAnswer({
+      year: 2022, day: 1, level: 1, answer: 'ASDF', authToken: 'asdf',
     });
+    expect(result).toEqual({ submissionResult: apiResponse });
+  });
+
+  test('returns on answer incorrect', async () => {
+    const apiResponse = { correct: false, message: 'that is wrong!' };
+    submitSolution.mockReturnValue(apiResponse);
+    const result = await submitPuzzleAnswer({
+      year: 2022, day: 1, level: 1, answer: 'ASDF', authToken: 'asdf',
+    });
+    expect(result).toEqual({ submissionResult: apiResponse });
   });
 });
