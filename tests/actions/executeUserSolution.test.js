745568e9b83615cebf941ca1f56e5f1d01059bf1
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-12 13:30:20 -0700

    rename part to level

--- tests/actions/executeUserSolution.test.js
+++ tests/actions/executeUserSolution.test.js
@@ -5,28 +5,30 @@ import { mockConfig, mockLogger } from '../mocks.js';
 
 // setup mocks
 mockLogger();
-const { getConfigValue } = mockConfig();
+mockConfig();
 const mockExecuter = jest.fn();
 jest.unstable_mockModule('src/solutions/index.js', () => ({ execute: mockExecuter }));
 
 // import after mocks set up
 const { executeUserSolution } = await import('../../src/actions/executeUserSolution.js');
 
-describe('actions', () => {
-  describe('links', () => {
-    afterEach(() => {
-      jest.useFakeTimers();
-      jest.resetAllMocks();
-    });
+describe('executeUserSolution()', () => {
+  afterEach(() => {
+    jest.useFakeTimers();
+    jest.resetAllMocks();
+  });
 
-    describe('executeUserSolution()', () => {
-      test('returns results', async () => {
-        const args = { day: 1, part: 1, input: 'ASDF' };
-        const executionResult = { answer: '1234', executionTimeNs: 1234 };
-        mockExecuter.mockResolvedValue(executionResult);
-        const result = await executeUserSolution(args);
-        expect(result).toEqual(executionResult);
-      });
-    });
+  test.each([
+    null, undefined,
+  ])('throws if question is %s', async (question) => {
+    await expect(async () => executeUserSolution(question)).rejects.toThrow();
+  });
+
+  test('returns results', async () => {
+    const args = { day: 1, level: 1, input: 'ASDF' };
+    const executionResult = { answer: '1234', executionTimeNs: 1234 };
+    mockExecuter.mockResolvedValue(executionResult);
+    const result = await executeUserSolution(args);
+    expect(result).toEqual(executionResult);
   });
 });
