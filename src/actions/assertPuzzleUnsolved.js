f2d4d7d8839031d366b0cc5b17bab0c4a19053d7
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-12 13:26:47 -0700

    rename part to level

--- src/actions/assertPuzzleUnsolved.js
+++ src/actions/assertPuzzleUnsolved.js
@@ -5,8 +5,8 @@ import { logger } from '../logger.js';
  * Returns true if the user has not solved the puzzle
  * Returns false if the user has solved the puzzle.
  */
-export const assertPuzzleUnsolved = async ({ year, day, part } = {}) => {
-  if (await puzzleHasBeenSolved(year, day, part)) {
+export const assertPuzzleUnsolved = async ({ year, day, level } = {}) => {
+  if (await puzzleHasBeenSolved(year, day, level)) {
     logger.error('You have already solved and submitted the correct answer this puzzle!');
     return false;
   }
