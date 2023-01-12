5460034da4be933b7b7a34f26624e4e9a054dfe3
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-12 13:36:17 -0700

    rename level to part

--- src/actions/storeFastestExecutionTime.js
+++ src/actions/storeFastestExecutionTime.js
@@ -4,11 +4,11 @@ import { setPuzzlesFastestRuntime } from '../statistics.js';
  * Update the fastest execution time for this puzzle with the new value.
  */
 export const storeFastestExecutionTime = async ({
-  year, day, part, executionTimeNs,
+  year, day, level, executionTimeNs,
 } = {}) => {
   if (executionTimeNs == null) {
     throw new Error('null or undefined execution time');
   }
 
-  await setPuzzlesFastestRuntime(year, day, part, executionTimeNs);
+  await setPuzzlesFastestRuntime(year, day, level, executionTimeNs);
 };
