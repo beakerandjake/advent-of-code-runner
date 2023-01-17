7d79602baf2059ed8c64a1b54937a2f981760ad0
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-17 11:28:03 -0700

    add link

---
+++ src/actions/saveCompletionTableToReadme.js
@@ -0,0 +1,10 @@
+import { pathExists } from 'fs-extra/esm';
+import { getConfigValue } from '../config.js';
+import { logger } from '../logger.js';
+
+/**
+ * Saves a table to the users readme which shows the users progress for the year.
+ */
+export const saveCompletionTableToReadme = async () => {
+  console.log('doing the thing!');
+};
