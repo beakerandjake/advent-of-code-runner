76a757a67f499f56fe12150d1d61892721ed3aaa
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-17 12:21:33 -0700

    grabbing completion data, start making table

--- src/actions/saveCompletionTableToReadme.js
+++ src/actions/saveCompletionTableToReadme.js
@@ -1,10 +1,37 @@
 import { pathExists } from 'fs-extra/esm';
 import { getConfigValue } from '../config.js';
 import { logger } from '../logger.js';
+import { getPuzzleCompletionData } from '../statistics.js';
+
+/**
+ * Generates a markdown table from the years data.
+ */
+const generateTable = async (year, completionData) => {
+  const headerRows = [
+    ['Puzzle', 'Solved', 'Attempts', 'Runtime'],
+    ['---', '---', '---', '---'],
+  ];
+
+  return '';
+};
 
 /**
  * Saves a table to the users readme which shows the users progress for the year.
  */
-export const saveCompletionTableToReadme = async () => {
-  console.log('doing the thing!');
+export const saveCompletionTableToReadme = async ({ year } = {}) => {
+  if (year == null) {
+    throw new Error('null or undefined year');
+  }
+
+  const completionData = await getPuzzleCompletionData(year);
+
+  if (completionData.length === 0) {
+    logger.festive('You have not submitted any puzzles yet, please run this command after submitting a puzzle');
+    return false;
+  }
+
+  const tableText = await generateTable(year, completionData);
+
+  console.log(tableText);
+  return true;
 };
