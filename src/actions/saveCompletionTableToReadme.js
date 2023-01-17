eef25a895a46e1d39cfe03277cfaaed292926e26
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-17 13:10:35 -0700

    testing html table instead of markdown

--- src/actions/saveCompletionTableToReadme.js
+++ src/actions/saveCompletionTableToReadme.js
@@ -2,17 +2,60 @@ import { pathExists } from 'fs-extra/esm';
 import { getConfigValue } from '../config.js';
 import { logger } from '../logger.js';
 import { getPuzzleCompletionData } from '../statistics.js';
+import { humanizeDuration } from '../formatting.js';
+
+
+
+const td = (value) => `<td>${value}</td>`;
+const th = (value, { colspan }) => `<th${colspan ? ` colspan=${colspan}` : ''}>${value}</th>`;
+const tr = (values) => `<tr>${values.join('')}</tr>`;
+
+/**
+ * Generates the text for the name column.
+ * @private
+ */
+export const mapNamedColumn = ({ day, level }) => `${day}.${level}`;
+
+/**
+ * Generates the text for the solved column.
+ * @private
+ */
+export const mapSolvedColumn = ({ solved }) => (solved ? '✓' : '');
+
+/**
+ * Generates the text for the attempt column.
+ * @private
+ */
+export const mapAttemptColumn = ({ numberOfAttempts }) => `${numberOfAttempts}`;
+
+/**
+ * Generates the text for the runtime column.
+ * @private
+ */
+export const mapRuntimeColumn = ({ runtimeNs }) => {
+  if (runtimeNs == null || runtimeNs === '') {
+    return '';
+  }
+
+  return humanizeDuration(runtimeNs);
+};
 
 /**
  * Generates a markdown table from the years data.
  */
 const generateTable = async (year, completionData) => {
-  const headerRows = [
-    ['Puzzle', 'Solved', 'Attempts', 'Runtime'],
-    ['---', '---', '---', '---'],
-  ];
+  const names = completionData.map(mapNamedColumn);
+  const solved = completionData.map(mapSolvedColumn);
+  const attempts = completionData.map(mapAttemptColumn);
+  const runtimes = completionData.map(mapRuntimeColumn);
 
-  return '';
+  const headerRow = tr(['Puzzle', 'Solved', 'Attempts', 'Runtime'].map(th));
+  const puzzleRows = completionData.map(
+    (_, index) => tr([names[index], solved[index], attempts[index], runtimes[index]].map(td)),
+  );
+  const averageRow = tr([th('Average', { colspan: 2 }), td('asdf'), td('asdf')]);
+
+  return [headerRow, ...puzzleRows, averageRow].join('');
 };
 
 /**
