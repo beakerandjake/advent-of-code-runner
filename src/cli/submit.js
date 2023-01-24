ff314f7751d2a1d74f7ffd8acd6f881f87331ecd
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-24 11:23:41 -0700

    merge submit and autosubmit

--- src/cli/submit.js
+++ src/cli/submit.js
@@ -1,46 +1,67 @@
 import { Command } from 'commander';
 import { createChain } from '../actions/actionChain.js';
-import * as links from '../actions/index.js';
+import * as actions from '../actions/index.js';
 import { dayArgument, levelArgument } from './arguments.js';
 
 /**
- * The links which together make up the submit action.
+ * The common actions between the 'submit' and 'autosubmit' commands
  */
-export const submitLinks = [
-  links.assertInitialized,
-  links.getYear,
-  links.outputPuzzleLink,
-  links.assertPuzzleUnlocked,
-  links.assertPuzzleLevelMet,
-  links.assertPuzzleUnsolved,
-  links.getAuthenticationToken,
-  links.getPuzzleInput,
-  links.executeUserSolution,
-  links.assertAnswerNotPreviouslySubmitted,
-  links.submitPuzzleAnswer,
-  links.storeSubmittedAnswer,
-  links.assertAnswerCorrect,
-  links.storeFastestRuntime,
+const submitActions = [
+  actions.outputPuzzleLink,
+  actions.assertPuzzleUnlocked,
+  actions.assertPuzzleLevelMet,
+  actions.assertPuzzleUnsolved,
+  actions.getAuthenticationToken,
+  actions.getPuzzleInput,
+  actions.executeUserSolution,
+  actions.assertAnswerNotPreviouslySubmitted,
+  actions.submitPuzzleAnswer,
+  actions.storeSubmittedAnswer,
+  actions.assertAnswerCorrect,
+  actions.storeFastestRuntime,
+  actions.assertReadmeExists,
+  actions.saveCompletionTableToReadme,
 ];
 
 /**
- * "compile" the links into the submit action.
+ * Submit a specific puzzle.
  */
-const actionChain = createChain(submitLinks);
+const submit = createChain([
+  actions.assertInitialized,
+  actions.getYear,
+  ...submitActions,
+]);
 
 /**
- * Execute the users solution then submit their answer to advent of code
- * @param {Number} day
- * @param {Number} level
+ * Finds the next unsolved puzzle and then submits it.
  */
-const submit = async (day, level) => actionChain({ day, level });
+const autoSubmit = createChain([
+  actions.assertInitialized,
+  actions.getYear,
+  actions.getNextUnsolvedPuzzle,
+  ...submitActions,
+]);
 
 /**
  * Command which allows the user to submit a puzzles answer to advent of code.
  */
 export const submitCommand = new Command()
   .name('submit')
-  .description('Run the solution for the puzzle, then submit the answer to advent of code.')
+  .description('Runs your solution for a puzzle and submits the answer to advent of code.`')
+  .addHelpText('after', `
+Example Calls:
+  submit               (Finds and submits your next unsolved puzzle)
+  submit [day]         (Submits level one of the specified days puzzle)
+  submit [day] [level] (Submits the puzzle for the specified day and level)
+    `)
   .addArgument(dayArgument)
   .addArgument(levelArgument)
-  .action(submit);
+  .action(async (day, level) => {
+    if (day == null && level == null) {
+      await autoSubmit({});
+    } else if (day != null && level == null) {
+      await submit({ day, level: 1 });
+    } else {
+      await submit({ day, level });
+    }
+  });
