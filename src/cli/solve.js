a58dd7166400c429232801f527622bd3c36b17ff
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-26 12:19:32 -0700

    remove unused import

--- src/cli/solve.js
+++ src/cli/solve.js
@@ -2,7 +2,6 @@ import { Command } from 'commander';
 import { createChain } from '../actions/actionChain.js';
 import * as actions from '../actions/index.js';
 import { dayArgument, levelArgument } from './arguments.js';
-import { tryToSaveProgressTableToReadme } from './stats.js';
 
 /**
  * The common actions between the 'solve' and 'autosolve' commands
