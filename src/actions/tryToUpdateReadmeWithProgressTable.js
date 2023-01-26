7023d03936f89d31ffa2880509e2ed4f5dd0c3b0
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-26 11:14:12 -0700

    ignore action for coverage

--- src/actions/tryToUpdateReadmeWithProgressTable.js
+++ src/actions/tryToUpdateReadmeWithProgressTable.js
@@ -8,6 +8,7 @@ import { not } from './logical.js';
 /**
  * Updates the users README with the progress table unless feature is disabled in config.
  */
+/* istanbul ignore next */
 export const tryToUpdateReadmeWithProgressTable = async (args) => {
   // this is a special action which runs its own action chain
   // expecting this action to be run in a parent chain.
