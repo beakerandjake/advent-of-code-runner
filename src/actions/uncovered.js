20d65e4d626dc5e7f3561072f8d90cdcef0f95d2
beakerandjake <beakerandjake@users.noreply.github.com>
2023-01-31 13:18:07 -0700

    add uncovered code

---
+++ src/actions/uncovered.js
@@ -0,0 +1,44 @@
+export const uncovered = async (year) => {
+  if (year === 2022) {
+    throw new Error();
+  }
+
+  if (year == null) {
+    throw new Error();
+  }
+
+  const z = 20;
+  const q = z + year;
+  const p = q * z;
+  console.log(p);
+};
+
+export const uncovered2 = async (year) => {
+  if (year === 2022) {
+    throw new Error();
+  }
+
+  if (year == null) {
+    throw new Error();
+  }
+
+  const z = 20;
+  const q = z + year;
+  const p = q * z;
+  console.log(p);
+};
+
+export const uncovered3 = async (year) => {
+  if (year === 2022) {
+    throw new Error();
+  }
+
+  if (year == null) {
+    throw new Error();
+  }
+
+  const z = 20;
+  const q = z + year;
+  const p = q * z;
+  console.log(p);
+};
