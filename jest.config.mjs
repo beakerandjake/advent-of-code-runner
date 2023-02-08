8151ab261f1a0983fd0aab31e5f0de0d1da8a279
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-08 13:19:28 -0700

    undo last commit

--- jest.config.mjs
+++ jest.config.mjs
@@ -161,10 +161,9 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/develop/"
-  ],
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
