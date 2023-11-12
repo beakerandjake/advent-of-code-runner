820b23812ff60620b6065a8001335388442b6030
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-12 13:45:06 -0700

    add template solution for day 25

---
+++ templates/template-solution-day25.js
@@ -0,0 +1,18 @@
+/**
+ * Contains solutions for Day 25
+ * Puzzle Description: https://adventofcode.com/{{year}}/day/25
+ * 
+ * Day 25 only has a level one problem that is solvable.
+ * To solve level two visit the advent of code website.
+ */
+
+/**
+ * Returns the solution for level one of this puzzle.
+ * @param {Object} args - Provides both raw and split input.
+ * @param {String} args.input - The original, unparsed input string.
+ * @param {String[]} args.lines - Array containing each line of the input string.
+ * @returns {Number|String}
+ */
+export const levelOne = ({ input, lines }) => {
+  // your code here
+};
