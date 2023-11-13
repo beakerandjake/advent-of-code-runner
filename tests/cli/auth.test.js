7058bfbb038af88af9e2165b5f742144f44d2de4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-13 10:06:34 -0700

    update auth test

--- tests/cli/auth.test.js
+++ tests/cli/auth.test.js
@@ -1,37 +1,50 @@
 import { describe, jest, test, afterEach } from '@jest/globals';
-import { mockLogger } from '../mocks.js';
+import { mockLogger, mockCommander } from '../mocks.js';
 
 // setup mocks.
 mockLogger();
-// jest.unstable_mockModule('commander', () => ({ Command: jest.fn() }));
-jest.unstable_mockModule('src/actions/assertInitialized.js', () => ({
+mockCommander();
+jest.unstable_mockModule('src/actions/index.js', () => ({
   assertInitialized: jest.fn(),
-}));
-jest.unstable_mockModule('src/actions/assertUserConfirmation.js', () => ({
   assertUserConfirmation: jest.fn(),
-}));
-jest.unstable_mockModule('src/actions/getAnswersFromUser.js', () => ({
   getAnswersFromUser: jest.fn(),
+  and:
+    (a, b) =>
+    async (...args) =>
+      (await a(...args)) && b(...args),
+}));
+jest.unstable_mockModule('src/actions/actionChain.js', () => ({
+  createChain: (links) => async (args) => {
+    let currentArgs = args;
+    for (const link of links) {
+      const result = await link(args);
+      if (result === false) {
+        break;
+      }
+      if (result !== true && result !== undefined) {
+        currentArgs = { ...currentArgs, ...result };
+      }
+    }
+  },
+}));
+jest.unstable_mockModule('src/festive.js', () => ({
+  festiveStyle: jest.fn(),
+  printFestiveTitle: jest.fn(),
+}));
+jest.unstable_mockModule('src/initialize/index.js', () => ({
+  createDotEnv: jest.fn(),
 }));
 jest.unstable_mockModule('src/validation/userFilesExist.js', () => ({
   dotEnvExists: jest.fn(),
 }));
-jest.unstable_mockModule('src/festive.js', () => ({
-  festiveEmoji: jest.fn(),
-  festiveStyle: jest.fn(),
-  printFestiveTitle: jest.fn(),
-}));
-jest.unstable_mockModule('src/initialize/index.js', () => ({ createDotEnv: jest.fn() }));
 
 // import after mocks are setup.
-const { assertInitialized } = await import('../../src/actions/assertInitialized.js');
-const { assertUserConfirmation } = await import(
-  '../../src/actions/assertUserConfirmation.js'
+const { assertInitialized, assertUserConfirmation, getAnswersFromUser } = await import(
+  '../../src/actions/index.js'
 );
-const { getAnswersFromUser } = await import('../../src/actions/getAnswersFromUser.js');
 const { createDotEnv } = await import('../../src/initialize/index.js');
 const { dotEnvExists } = await import('../../src/validation/userFilesExist.js');
-const { auth } = await import('../../src/cli/auth.js');
+const { authAction } = await import('../../src/cli/auth.js');
 
 describe('auth command', () => {
   afterEach(() => {
@@ -40,32 +53,32 @@ describe('auth command', () => {
 
   test('bails if user has not initialized', async () => {
     assertInitialized.mockResolvedValue(false);
-    await auth();
+    await authAction();
     expect(createDotEnv).not.toHaveBeenCalled();
   });
 
   test('confirms with user if .env file exists', async () => {
     assertInitialized.mockResolvedValue(true);
     dotEnvExists.mockResolvedValue(true);
-    assertUserConfirmation.mockReturnValue(() => Promise.resolve(false));
-    await auth();
+    assertUserConfirmation.mockResolvedValue(false);
+    await authAction();
     expect(assertUserConfirmation).toHaveBeenCalled();
   });
 
   test('bails if user does not confirm', async () => {
     assertInitialized.mockResolvedValue(true);
     dotEnvExists.mockResolvedValue(true);
-    assertUserConfirmation.mockReturnValue(() => Promise.resolve(false));
-    await auth();
+    assertUserConfirmation.mockResolvedValue(false);
+    await authAction();
     expect(createDotEnv).not.toHaveBeenCalled();
   });
 
   test('creates dotenv if user confirms', async () => {
     assertInitialized.mockResolvedValue(true);
     dotEnvExists.mockResolvedValue(true);
-    assertUserConfirmation.mockReturnValue(() => Promise.resolve(true));
-    getAnswersFromUser.mockReturnValue(() => Promise.resolve({}));
-    await auth();
+    assertUserConfirmation.mockResolvedValue(true);
+    getAnswersFromUser.mockResolvedValue({});
+    await authAction();
     expect(createDotEnv).toHaveBeenCalled();
   });
 });
