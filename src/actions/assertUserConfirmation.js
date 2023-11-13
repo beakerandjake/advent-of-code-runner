954feba5c3cf8ad40bd82b1bacfc57e9f874f4de
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-12 20:50:38 -0700

    update auth action to use new inquirer

--- src/actions/assertUserConfirmation.js
+++ src/actions/assertUserConfirmation.js
@@ -1,22 +1,15 @@
-import inquirer from 'inquirer';
+import { confirm } from '@inquirer/prompts';
 
 /**
  * Creates a link which when invoked will ask the user the specified question.
  * The link will return a boolean value based on if the user answered yes to the question.
- * @param {Object} question - The question passed to inquirer
+ * @param {Object} confirmQuestion - The question passed to the confirm prompt
+ * @param {string} confirmQuestion.message - The question to ask
+ * @param {boolean} confirmQuestion.default - The default answer (true or false)
  */
-export const assertUserConfirmation = (question) => {
-  if (question == null) {
-    throw new Error('null or undefined question');
+export const assertUserConfirmation = async ({ confirmQuestion }) => {
+  if (!confirmQuestion || !confirmQuestion.message) {
+    throw new Error('must specify a message');
   }
-
-  // create a variable for this fn instead of just returning the fn
-  // this gives the fn a .name property and makes debugging easier.
-  const _ = {
-    assertUserConfirmation: async () => {
-      const { confirmed } = await inquirer.prompt({ ...question, type: 'confirm' });
-      return confirmed;
-    },
-  };
-  return _.assertUserConfirmation;
+  return confirm(confirmQuestion);
 };
--- src/actions/getAnswersFromUser.js
+++ src/actions/getAnswersFromUser.js
@@ -1,20 +1,34 @@
-import inquirer from 'inquirer';
+import { confirm, select, password } from '@inquirer/prompts';
+
+/**
+ * Prompt the user with an inquirer question.
+ */
+const prompt = (type, args) => {
+  switch (type) {
+    case 'confirm':
+      return confirm(args);
+    case 'select':
+      return select(args);
+    case 'password':
+      return password(args);
+    default:
+      throw new Error(`unsupported question type: ${type}`);
+  }
+};
 
 /**
  * Creates a link which, when invoked will ask the user the specified questions.
  * The link will add the user answers to the args.
  * @param {Object[]} questions - The inquirer.js questions to ask the user
  */
-export const getAnswersFromUser = (questions = []) => {
-  if (questions == null) {
+export const getAnswersFromUser = async ({ questions }) => {
+  if (!questions) {
     throw new Error('null or undefined question');
   }
-
-  // create a variable for this fn instead of just returning the fn
-  // this gives the fn a .name property and makes debugging easier.
-  const _ = {
-    getAnswersFromUser: async () => ({ answers: await inquirer.prompt(questions) }),
-  };
-
-  return _.getAnswersFromUser;
+  const answers = {};
+  for (const [key, { type, ...args }] of Object.entries(questions)) {
+    // eslint-disable-next-line no-await-in-loop
+    answers[key] = await prompt(type, args);
+  }
+  return answers;
 };
