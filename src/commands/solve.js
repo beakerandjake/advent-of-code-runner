11de554ec89b246f4fef4e6309428ffa16e27af2
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-14 13:42:38 -0700

    clean up

--- src/commands/solve.js
+++ src/commands/solve.js
@@ -39,15 +39,16 @@ const executeUserSolution = async (day, level, input) => {
     logger.festive('Your code is still running, press Ctrl+C to cancel...');
   }, getConfigValue('solutionRunner.cancelMessageDelayMs'));
 
-  const { answer, runtimeNs } = await execute(day, level, input);
+  const result = await execute(day, level, input);
 
   clearTimeout(timeout);
+
   logger.festive(
     'You answered: %s (solved in %s)',
-    answer,
-    humanizeDuration(runtimeNs)
+    result.answer,
+    humanizeDuration(result.runtimeNs)
   );
-  return { answer, runtimeNs };
+  return result;
 };
 
 /**
@@ -65,17 +66,14 @@ export const tryToSolvePuzzle = async (year, day, level) => {
   if (!(await requiredLevelsHaveBeenSolved(year, day, level))) {
     throw new PuzzleLevelNotMetError(day, level);
   }
-
-  // log puzzle url to console.
   const clickable = makeClickableLink('Puzzle', puzzleBaseUrl(year, day));
   logger.festive(`${clickable} (Year: ${year} Day: ${day} Level: ${level})`);
-
-  // load the puzzle input.
   logger.festive('Loading puzzle input.');
-  const input = await getPuzzleInput(year, day, level);
-
-  // run the users code and return the result.
-  return executeUserSolution(day, level, input);
+  return executeUserSolution(
+    day,
+    level,
+    await getPuzzleInput(year, day, level)
+  );
 };
 
 /**
@@ -85,8 +83,6 @@ export const tryToSolvePuzzle = async (year, day, level) => {
 const answerIsCorrect = async (year, day, level, answer) => {
   const correctAnswer = await getCorrectAnswer(year, day, level);
   if (correctAnswer && !answersEqual(answer, correctAnswer)) {
-    // if new answer doesn't match correct answer
-    // warn user that something might have broken.
     logger.warn(
       'Puzzle previously submitted, but answer: "%s" doesn\'t match correct answer: "%s"',
       answer,
@@ -149,59 +145,9 @@ export const solveAction = async (day, level) => {
   if (!(await dataFileExists())) {
     throw new DirectoryNotInitializedError();
   }
-  // solve or auto solve based on provided parameters
   if (!day && !level) {
     await autoSolve(await getYear());
   } else {
     await solve(await getYear(), day, level || 1);
   }
 };
-
-// /**
-//  * The common actions between the 'solve' and 'autosolve' commands
-//  */
-// const solveActions = [
-//   actions.assertPuzzleHasLevel,
-//   actions.outputPuzzleLink,
-//   actions.assertPuzzleUnlocked,
-//   actions.assertPuzzleLevelMet,
-//   actions.getPuzzleInput,
-//   actions.executeUserSolution,
-//   actions.assertAnswerCorrect,
-//   actions.tryToUpdateFastestRuntime,
-//   actions.tryToUpdateReadmeWithProgressTable,
-// ];
-
-// /**
-//  * Solves a specific puzzle.
-//  */
-// // prettier-ignore
-// const solve = createChain([
-//   actions.assertInitialized,
-//   actions.getYear,
-//   ...solveActions,
-// ]);
-
-// /**
-//  * Finds the next unsolved puzzle and then solves it.
-//  */
-// const autoSolve = createChain([
-//   actions.assertInitialized,
-//   actions.getYear,
-//   actions.getNextUnsolvedPuzzle,
-//   ...solveActions,
-// ]);
-
-// /**
-//  * The action that is invoked by commander.
-//  * @private
-//  */
-// export const solveAction = async (day, level) => {
-//   if (day == null && level == null) {
-//     await autoSolve({});
-//   } else if (day != null && level == null) {
-//     await solve({ day, level: 1 });
-//   } else {
-//     await solve({ day, level });
-//   }
-// };
