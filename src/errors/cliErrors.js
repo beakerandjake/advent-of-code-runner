5a8511c04fb99009044779f7af720b9a8681f3c4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-13 17:27:44 -0700

    add error

--- src/errors/cliErrors.js
+++ src/errors/cliErrors.js
@@ -1,11 +1,21 @@
 import { UserError } from './userError.js';
 
 /**
- * Error raised if the Solution Worker thread exits without posting an answer message.
+ * Error raised if users directory is not initialized.
  */
 export class DirectoryNotInitializedError extends UserError {
   constructor(...args) {
-    super("directory not initialized, run 'init' command.", ...args);
+    super('directory not initialized, run init command.', ...args);
+    this.name = 'DirectoryNotInitializedError';
+  }
+}
+
+/**
+ * Error raised if required file is not found in users directory.
+ */
+export class FileNotFoundError extends UserError {
+  constructor(file, ...args) {
+    super(`could not find file: ${file} in project directory.`, ...args);
     this.name = 'DirectoryNotInitializedError';
   }
 }
