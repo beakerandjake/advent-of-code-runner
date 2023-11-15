d20a98ad77f45e93182a8480d3823b649c205719
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-15 13:43:05 -0700

    remove unused file

--- src/solutions/index.js
+++
@@ -1,9 +0,0 @@
-import { execute } from './solutionRunner.js';
-
-/**
- * If start to support different "runtimes" we can add logic to conditionally export
- * solution runners. For example we could support user solutions written in different
- * languages such as python.
- */
-
-export { execute };
