9c1f5ceaa7510f5d9bf60ea22808a17a5c6825df
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-15 13:40:40 -0700

    remove unused code

--- src/validation/validateArgs.js
+++
@@ -1,8 +0,0 @@
-import { getConfigValue } from '../config.js';
-
-/**
- * Does the year match one where advent of code was held?
- * @param {Number} year
- */
-export const yearIsValid = (year) =>
-  getConfigValue('aoc.validation.years').includes(year);
