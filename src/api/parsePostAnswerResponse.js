923c0f5d032d14c693dc7da511ec506d7f62b861
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-17 13:14:32 -0700

    scaffold parsers

--- src/api/parsePostAnswerResponse.js
+++ src/api/parsePostAnswerResponse.js
@@ -1,20 +1,73 @@
 import { ParsePostAnswerResponseError } from '../errors/apiErrors.js';
 
+/**
+ * Returns a parser which matches an "answer too low" response.
+ */
 const answerTooLow = () => {
   const matcher = (response) => {};
   return [matcher, false, () => 'Answer was too low'];
 };
 
-const parsers = [answerTooLow()];
+/**
+ * Returns a parser which matches an "answer too high" response.
+ */
+const answerTooHigh = () => {
+  const matcher = (response) => {};
+  return [matcher, false, () => 'Answer was too high.'];
+};
 
+/**
+ * Returns a parser which matches a "not the right answer" response.
+ */
+const notTheRightAnswer = () => {
+  const matcher = (response) => {};
+  return [matcher, false, () => ''];
+};
+
+/**
+ * Returns a parser which matches a "answered too recently" response.
+ */
+const answeredTooRecently = () => {
+  const matcher = (response) => {};
+  return [matcher, false, () => ''];
+};
+
+/**
+ * Returns a parser which matches a not 'solving at the right level' response.
+ */
+const incorrectLevel = () => {
+  const matcher = (response) => {};
+  return [matcher, false, () => ''];
+};
+
+/**
+ * Returns a parser which matches a not 'solving at the right level' response.
+ */
+const correctAnswer = () => {
+  const matcher = (response) => {};
+  return [matcher, false, () => ''];
+};
+
+const parsers = [
+  incorrectLevel(),
+  answeredTooRecently(),
+  answerTooLow(),
+  answerTooHigh(),
+  notTheRightAnswer(),
+  correctAnswer(),
+];
+
+/**
+ * Parses the raw html response from the html and returns the result of the submission.
+ * @param {string} response - The html that the server responded with.
+ * @returns {Promise<{correct:boolean,message:string}>}
+ */
 export const parsePostAnswerResponse = async (response) => {
   for (const [matchFn, correct, messageFn] of parsers) {
     if (matchFn(response)) {
       return { correct, message: messageFn(response) };
     }
   }
-
-  // If failed to match a parser then write out the full response
-  // to a temporary html file on the users system.
+  // TODO, write out response html to a temp folder and update message
   throw new ParsePostAnswerResponseError();
 };
